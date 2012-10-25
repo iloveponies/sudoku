a48f69044db99a93933d7c51fcbd7902b581407e
Daria Antonova <daria.antonova@helsinki.fi>
2012-10-25 21:16:23 +0300

    first 8 done

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -4,28 +4,46 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[row col] coord]
+    (set (get board row))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[row col] coord]
+    (set (map (fn [x] (get x col)) board))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [x coords
+        y coords]
+    [x y]))
 
 (defn block-values [board coord]
-  nil)
+	(let [[row col] coord
+          top-left-corner-x (* (quot row 3) 3) 
+          top-left-corner-y (* (quot col 3) 3)
+          block-range-x (vec (range top-left-corner-x (+ top-left-corner-x 3)))
+          block-range-y (vec (range top-left-corner-y (+ top-left-corner-y 3)))]
+      (set (for [x block-range-x
+            y block-range-y]
+        (get-in board [x y])))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (let [valid-vals (set (range 1 10))
+        row-vals (row-values board coord)
+        col-vals (col-values board coord)
+        block-vals (block-values board coord)
+        restricted-vals (set/union row-vals col-vals block-vals)]
+    (if(has-value? board coord) 
+      #{}
+      (set/difference valid-vals restricted-vals))))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (set (flatten board)) 0)))
 
 (defn rows [board]
   nil)
