75a89f8397efe05175484e29cda8622db6e9809f
Daria Antonova <daria.antonova@helsinki.fi>
2012-10-26 12:08:07 +0300

    tests passed

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -49,30 +49,44 @@
   (for [x board] (set x)))
 
 (defn valid-rows? [board]
-  ;(let [valid-row (range 1 10)]
+  (let [valid-row (set (range 1 10))]
   ;  (empty? (filter (fn [row] (not (== row valid-row))) board))))
-  nil)
+    (every? (fn [x] (= (set x) valid-row)) (rows board))))
 
 (defn cols [board]
   (for [x (range 0 9)] (col-values board [0 x])))
 
 (defn valid-cols? [board]
-  nil)
+   (let [valid-col (set (range 1 10))]
+    (every? (fn [x] (= (set x) valid-col)) (cols board))))
 
 (defn blocks [board]
-  nil)
+  (for [x (range 0 9 3)
+        y (range 0 9 3)]
+    (block-values board [x y])))
 
 (defn valid-blocks? [board]
-  nil)
+   (let [valid-block (set (range 1 10))]
+    (every? (fn [x] (= (set x) valid-block)) (blocks board))))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (let [empty-point? (fn [a] (let [[x y] a] (not (has-value? board [x y]))))]
+    (first (filter empty-point? (coord-pairs (range 0 9))))))
 
-(defn solve [board]
-  nil)
+(defn solve-helper [board]
+  (let [empty-point (find-empty-point board)]
+    (if (filled? board)
+      (if (valid-solution? board)
+        [board]
+        [])
+      (for [x (valid-values-for board empty-point)
+            solution (solve-helper (assoc-in board empty-point x))]
+        solution))))
+
+(defn solve [board] (first (solve-helper board)))
