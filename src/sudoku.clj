f87685bcdf5f2d8ccff9e93a881fa2d156500257
Topi Talvitie <topi.talvitie@iki.fi>
2013-10-21 00:03:44 +0300

    ☃

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,98 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [u v]]
+  (into #{} (board u)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [u v]]
+  (into #{}
+        (map (fn [row] (row v))
+             board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [a coords
+        b coords]
+    [a b]))
+
+(defn block-topleft [[u v]]
+  [(* 3 (int (/ u 3))) (* 3 (int (/ v 3)))])
 
 (defn block-values [board coord]
-  nil)
+  (let [[tu tv] (block-topleft coord)]
+    (into #{}
+          (for [[du dv] (coord-pairs [0 1 2])]
+            (let [u (+ tu du)
+                  v (+ tv dv)]
+              (value-at board [u v]))))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values
+                    (set/union (row-values board coord)
+                               (col-values board coord)
+                               (block-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (every? (comp not zero?)
+          (for [coord (coord-pairs (range 0 9))]
+            (value-at board coord))))
 
 (defn rows [board]
-  nil)
+  (map (partial row-values board)
+       (map vector (range 0 9) (repeat 9 0))))
 
 (defn valid-rows? [board]
-  nil)
+  (every? (comp (partial == 9) count)
+          (rows board)))
 
 (defn cols [board]
-  nil)
+  (map (partial col-values board)
+       (map vector (repeat 9 0) (range 0 9))))
 
 (defn valid-cols? [board]
-  nil)
+  (every? (comp (partial == 9) count)
+          (cols board)))
 
 (defn blocks [board]
-  nil)
+  (map (partial block-values board)
+       (coord-pairs [0 3 6])))
 
 (defn valid-blocks? [board]
-  nil)
+  (every? (comp (partial == 9) count)
+          (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (every? (fn [f] (f board))
+          [filled?
+           valid-rows?
+           valid-cols?
+           valid-blocks?]))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (some (fn [coord]
+          (if (has-value? board coord)
+            nil
+            coord))
+        (coord-pairs (range 0 9))))
 
 (defn solve [board]
-  nil)
+  (if (filled? board)
+    (if (valid-solution? board)
+      board
+      nil)
+    (let [coord (find-empty-point board)]
+      (some (fn [value]
+              (solve (set-value-at board coord value)))
+            (valid-values-for board coord)))))
