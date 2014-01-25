5ea15b13e791dc6c3107facc358e57ce6c001bb6
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2014-01-25 03:32:16 +0200

    :(

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -2,57 +2,104 @@
   (:require [clojure.set :as set]))
 
 (def board identity)
+(def all-values #{1 2 3 4 5 6 7 8 9})
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (= 0 (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (set (get board (first coord))))
 
 (defn col-values [board coord]
-  nil)
+  (let [xCoord (second coord)]
+    (reduce
+     (fn [x y] (conj x (get y xCoord)))
+     #{} board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for
+    [coord1 coords
+     coord2 coords]
+    (vector coord1 coord2)))
+
+(defn clamp-coord-topleft [coord]
+  (let [[x y] coord]
+    (vector (* 3(int (/ x 3))) (* 3(int (/ y 3))))))
 
 (defn block-values [board coord]
-  nil)
+  (let [[x y] (clamp-coord-topleft coord)]
+    (set (for [curX (vector x (+ x 1) (+ x 2))
+               curY (vector y (+ y 1) (+ y 2))]
+           (value-at board (vector curX curY))))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (cond
+   (has-value? board coord) #{}
+   :else (set/difference all-values (set/union (row-values board coord) (col-values board coord) (block-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (cond
+   (empty? board) true
+   (contains? (set (first board)) 0) false
+   :else (recur (rest board))))
 
 (defn rows [board]
-  nil)
+  (loop [a-vec []
+         i 0]
+    (cond
+     (= i 9) a-vec
+     :else (recur
+            (conj a-vec (set (row-values board (vector i 0))))
+            (+ i 1)))))
 
 (defn valid-rows? [board]
-  nil)
+  (every? (fn [x] (empty? (set/difference all-values x))) (rows board)))
 
 (defn cols [board]
-  nil)
+  (loop [a-vec []
+         i 0]
+    (cond
+     (= i 9) a-vec
+     :else (recur
+            (conj a-vec (set (col-values board (vector 0 i))))
+            (+ i 1)))))
 
 (defn valid-cols? [board]
-  nil)
+  (every? (fn [x] (empty? (set/difference all-values x))) (cols board)))
 
 (defn blocks [board]
-  nil)
+    (for [x [0 3 6]
+          y [0 3 6]]
+      (block-values board (vector x y))))
+
 
 (defn valid-blocks? [board]
-  nil)
+  (every? (fn [x] (empty? (set/difference all-values x))) (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (loop [y 0]
+    (cond
+     (= y 9) nil
+     (contains? (set (get board y)) 0) (vector y (.indexOf (get board y) 0))
+     :else (recur (+ y 1)))))
+
 
 (defn solve [board]
-  nil)
+  (if (filled? board)
+    (if (valid-solution? board) board nil)
+    (let [coord (find-empty-point board)]
+      (loop [remaining-values (valid-values-for board coord)]
+          (let [solution (solve (set-value-at board coord (first remaining-values)))]
+          (if (empty? solution) (recur (rest remaining-values)) nil))))))
+
+
+
