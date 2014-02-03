86c4cf0d4d646603591311ca6477269ef9169670
Jean-François Héon <jfheon@gmail.com>
2014-02-02 20:27:48 -0500

    I'm coming up

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,22 +4,43 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not= 0 (value-at board coord)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row-index _]]
+  (set (board row-index)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [_ column-index]]
+  (let [column (map (fn [row] (row column-index)) board)]
+    (set column)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [row coords
+        col coords]
+    [row col]))
+
+(defn round-multiple-of-three [n]
+  (* 3 (quot n 3)))
+
+(defn block-origin [[row col]]
+  [(round-multiple-of-three row)
+   (round-multiple-of-three col)])
+
+;todo defn block-coordinates
+
+(defn block-coordinates
+  "Get the block coordinates for a given coordinate."
+  [coord]
+  (let [[row-origin col-origin] (block-origin coord)]
+    (for [row (range row-origin (+ 3 row-origin))
+          col (range col-origin (+ 3 col-origin))]
+      [row col])))
 
 (defn block-values [board coord]
-  nil)
+  (let [coordinates (block-coordinates coord)]
+    (map (fn [coordinate] (value-at board coordinate)) coordinates)))
 
 (defn valid-values-for [board coord]
   nil)
