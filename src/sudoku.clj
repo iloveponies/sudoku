3b88af158eeee793fce13386db34b37cc0a35b59
Olli Piepponen <opieppo@gmail.com>
2014-02-27 10:22:44 +0700

    Solved some performance issues

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -1,20 +1,15 @@
 (ns sudoku
-  (:require [clojure.set :as set])
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [clojure.set :as set]))
 
 (def board identity)
 
 (def all-values #{1 2 3 4 5 6 7 8 9})
 
-(defn transpose [board]
-  (apply mapv vector board))
 (defn value-at [board coord]
   (get-in board coord))
 
 (defn has-value? [board coord]
-  (if (= (value-at board coord) 0)
-    false
-    true))
+  (not= (value-at board coord) 0))
 
 (defn row-values [board [row _]]
   (set (get board row)))
@@ -23,17 +18,8 @@
   (set (map #(get % col) board)))
 
 (defn coord-pairs [coord]
-  (for [r coord
-        c coord]
+  (for [r coord, c coord]
     [r c]))
-
-;; 0, 1, 2 => 0
-;; 3, 4, 5 => 1
-;; 6, 7, 8 => 2
-
-;; 1, 2, 3 => 1
-;; 4, 5, 6 => 2
-;; 7, 8, 9 => 3
 
 (defn block-values [board coord]
   (let [topleft (map #(* 3 (quot % 3)) coord)
@@ -51,22 +37,26 @@
       (set/difference all-values used-values))))
 
 (defn filled? [board]
-  (= (count (filter zero? (flatten board))) 0))
+  (every? (fn [x] (every? (fn [y] (> y 0)) x)) board))
 
 (defn rows [board]
-  (map #(into #{} (get board %)) (range 9)))
+  (map set board))
 
-(defn valid? [board vals]
+(defn- valid? [board vals]
   (every? #(= % all-values) vals))
+
 (defn valid-rows? [board]
-  (valid? board (rows board)))
+  (and (filled? board) (valid? board (rows board))))
+
+(defn- transpose [board]
+  (apply map vector board))
 
 (defn cols [board]
   (let [transposed (transpose board)]
-    (rows transposed)))
+    (map set transposed)))
 
 (defn valid-cols? [board]
-  (valid? board (cols board)))
+  (and (filled? board) (valid? board (cols board))))
 
 (defn blocks [board]
   (let [block-coords [[0 0] [0 3] [0 6]
@@ -75,7 +65,7 @@
     (map (partial block-values board) block-coords)))
 
 (defn valid-blocks? [board]
-  (valid? board (blocks board)))
+  (and (filled? board) (valid? board (blocks board))))
 
 (defn valid-solution? [board]
   (every? true? (map #(% board) [valid-rows? valid-cols? valid-blocks?])))
@@ -84,47 +74,15 @@
   (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  (let [inc-coord (fn inc-coord [coord]
-                    (cond
-                     (and (= (first coord) 8) (= (second coord) 8)) [0 0]
-                     (= (first coord) 8) [0 (second coord)]
-                     (= (second coord) 8) [(first coord) 0]
-                     :else [(first coord) (inc (second coord))]))]
-    (loop [coord [0 0]]
-      (cond
-       (filled? board) nil
-       (has-value? board coord) (recur (inc-coord coord))
-       :else coord))))
-
-(defn find-empty-points [board]
-  (let [all-coords (coord-pairs (range 9))]
-    (filter (complement (partial has-value? board)) all-coords)))
-
-;; (defn solve-helper [board empty-points]
-;;   (if (filled? board)
-;;     (if (valid-solution? board) [board] [])
-;;     (for [vv (valid-values-for board (first empty-point))
-;;           solution (solve-helper (set-value-at board empty-point vv))]
-;;       solution)))
+  (first (filter #(= 0 (value-at board %)) (coord-pairs (range 10)))))
 
 (defn solve [board]
-  nil)
-
-;; (defn solve [board]
-;;   (let [empty-points (find-empty-points board)
-;;         valid-values (map (partial valid-values-for board) empty-points)
-;;         potential-solutions (apply combo/cartesian-product valid-values)
-;;         with-coords (for [ps potential-solutions]
-;;                       (map-indexed #(vector (nth empty-points %1) %2) ps))]
-;;     (loop [tries with-coords]
-;;       (let [solution
-;;             (reduce (fn [board [coord value]]
-;;                       (set-value-at board coord value))
-;;                     board (first tries))]
-;;         (if (valid-solution? solution)
-;;           solution
-;;           (recur (rest tries)))))))
-
-;;;;;;;;;;;;;;;;;
-
-
+  (if (filled? board)
+    (if (valid-solution? board)
+      board
+      [])
+    (let [empty-point (find-empty-point board)
+          valid-values (valid-values-for board empty-point)]
+      (for [vv valid-values
+            solution (solve (set-value-at board empty-point vv))]
+        solution))))
