fbe4d06145b659f1e969d33cef041777771e1131
Rohit Sachdeva <growth@drinnovations.us>
2014-03-07 15:03:16 -0600

    Sudoku - Ex 1-2 - doing in parts, rest should fail

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,10 +4,10 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (if (zero? (value-at board coord)) false true))
 
 (defn row-values [board coord]
   nil)
