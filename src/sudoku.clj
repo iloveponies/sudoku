1bc76f4b05f4f40a8bf0395dc1b33aa7b8800894
Turjo Tuohiniemi <turjo.tuohiniemi@reaktor.fi>
2014-05-16 15:58:04 +0300

    eka vertsio

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,43 +4,92 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (> (value-at board coord) 0))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [y x]]
+  (loop [values #{}
+         a-col (get board y)]
+    (if (empty? a-col)
+      values
+      (recur (conj values (first a-col)) (rest a-col)))))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [y x]]
+  (loop [values #{}
+         a-row board]
+    (if (empty? a-row)
+      values
+      (recur (conj values (get (first a-row) x)) (rest a-row)))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [y coords
+        x coords]
+    [y x]))
 
-(defn block-values [board coord]
-  nil)
+(defn top-left-block-coord [y x]
+  (let [round-down (fn [value]
+                     (- value (mod value 3)))]
+    [(round-down y) (round-down x)]))
+
+(defn block-coords [[y x]]
+  (for [j (range 3)
+        i (range 3)]
+    [(+ j (get (top-left-block-coord y x) 0))
+     (+ i (get (top-left-block-coord y x) 1))]))
+
+(defn block-values-perfect [board coords]
+  (loop [values #{}
+         a-coord (block-coords coords)]
+    (if (empty? a-coord)
+      values
+      (recur (if (has-value? board (first a-coord))
+               (conj values (value-at board (first a-coord)))
+               values
+             ) (rest a-coord)))))
+
+(defn block-values [board coords]
+  (loop [values #{}
+         a-coord (block-coords coords)]
+    (if (empty? a-coord)
+      values
+      (recur (conj values (value-at board (first a-coord))) (rest a-coord)))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference #{0 1 2 3 4 5 6 7 8 9} (block-values board coord))))
 
 (defn filled? [board]
-  nil)
+  (loop [rows board]
+    (cond
+     (empty? (first rows))
+       true
+     (some zero? (first rows))
+       false
+     :else
+       (recur (rest rows)))))
 
 (defn rows [board]
-  nil)
+  (for [y (range 9)]
+    (row-values board [y 0])))
 
 (defn valid-rows? [board]
   nil)
 
 (defn cols [board]
-  nil)
+  (for [x (range 9)]
+    (col-values board [0 x])))
 
 (defn valid-cols? [board]
   nil)
 
 (defn blocks [board]
-  nil)
+  (for [b '([0 0] [0 3] [0 6]
+            [3 0] [3 3] [3 6]
+            [6 0] [6 3] [6 6])]
+    (block-values board b)))
 
 (defn valid-blocks? [board]
   nil)
