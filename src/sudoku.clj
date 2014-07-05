7cd97bd8b2fa883d4f5742d43dc4fb245c4437d4
Evan Kline <evkline@gmail.com>
2014-07-04 20:14:51 -0400

    sudoku

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,105 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (if (zero? (value-at board coord))
+    false
+    true))
 
 (defn row-values [board coord]
-  nil)
+  (set (board (first coord))))
 
 (defn col-values [board coord]
-  nil)
+  (reduce #(conj %1 (%2 (last coord))) #{} board))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [row coords
+        col coords]
+    (vector row col)))
+
+(defn block-top-left-pos [pos]
+  (cond
+    (<= pos 2) 0
+    (<= pos 5) 3
+    :else 6))
+
+(defn block-top-left-coord [coord]
+  (let [row (first coord)
+        col (last coord)]
+    (vector (block-top-left-pos row) (block-top-left-pos col))))
 
 (defn block-values [board coord]
-  nil)
+  (let [top-left (block-top-left-coord coord)
+        start-row (first top-left)
+        start-col (last top-left)
+        rows (range start-row (+ start-row 3))
+        cols (range start-col (+ start-col 3))
+        coords (for [rb rows
+                     cb cols]
+                 (vector rb cb))]
+    (->> coords
+         (map #(value-at board %))
+         set)))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values (set/union (row-values board coord)
+                                          (col-values board coord)
+                                          (block-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (not-any? #(contains? (set %) 0) board))
 
 (defn rows [board]
-  nil)
+  (map-indexed (fn [idx _] (row-values board [idx idx])) board))
 
 (defn valid-rows? [board]
-  nil)
+  (every? #(= all-values %) (rows board)))
 
 (defn cols [board]
-  nil)
+  (map-indexed (fn [idx _] (col-values board [idx idx])) board))
 
 (defn valid-cols? [board]
-  nil)
+  (every? #(= all-values %) (cols board)))
 
 (defn blocks [board]
-  nil)
+  (for [row (range 3)
+        col (range 3)]
+    (set (block-values board [(* row 3) (* col 3)]))))
 
 (defn valid-blocks? [board]
-  nil)
+  (every? #(= all-values %) (blocks board)))
+
 
 (defn valid-solution? [board]
-  nil)
+  (and
+    (valid-rows? board)
+    (valid-cols? board)
+    (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (->> (for [row (range (count all-values))
+        col (range (count all-values))]
+    [row col])
+       (filter #(zero? (value-at board %)))
+       first))
 
 (defn solve [board]
-  nil)
+  (letfn [(guess-next-move-board [coord value]
+            (set-value-at board coord value))
+          (guess-next-move-boards []
+            (let [coord (find-empty-point board)]
+              (map #(solve (guess-next-move-board coord %)) (valid-values-for board coord))))]
+    (cond
+      (valid-solution? board) board
+      (filled? board) nil
+      :else (some identity (guess-next-move-boards)))))
