f8dfa4e6d944d4871fbc06ca448d6670768dca52
Massimo Battestini <MassimoBattestini@powa.com>
2014-07-25 15:59:05 +0100

    partial

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,37 +4,51 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not= 0 (value-at board coord)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row col]]
+  (set (get board row)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [row col]]
+  (set (for [row (range 9)]
+    (value-at board [row col]))))
 
-(defn coord-pairs [coords]
-  nil)
+(defn coord-pairs [a-seq]
+  (apply vector (for [row a-seq
+                col a-seq]
+            [row col])))
 
 (defn block-values [board coord]
-  nil)
+  (let [top-left (fn[coord]
+                   (map #(* 3 (int (/ % 3))) coord))]
+    (set (map #(value-at board (map + % (top-left coord))) (coord-pairs [0 1 2])))))
+
+(def all-values #{1 2 3 4 5 6 7 8 9})
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (zero? (value-at board coord))
+    (let [block-set (block-values board coord)
+          row-set (row-values board coord)
+          col-set (col-values board coord)]
+      (set/difference all-values block-set row-set col-set))
+    #{}))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (reduce #(set/union %1 (set %2)) #{} board) 0)))
 
 (defn rows [board]
-  nil)
+  (apply vector (map set board)))
 
 (defn valid-rows? [board]
   nil)
 
 (defn cols [board]
-  nil)
+  (let [col-val (fn[col]
+                  (set (reduce #(cons (value-at board [%2 col]) %1) [] (range 9))))]
+    (reduce #(conj %1 (col-val %2)) [] (range 9))))
 
 (defn valid-cols? [board]
   nil)
