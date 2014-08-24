a2b5404c883f23e6b19eb507fc1892315572eb0a
Alex Podaras <apodaras@gmail.com>
2014-08-24 15:47:34 +0300

    Solve exercise 6

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -22,8 +22,16 @@
         col coords]
     [row col]))
 
+(defn block-pairs [[row col]]
+  (let [r-min (* 3 (quot row 3))
+        c-min (* 3 (quot col 3))
+        pairs (for [r (range r-min (+ 3 r-min))
+                    c (range c-min (+ 3 c-min))]
+                [r c])]
+    pairs))
+
 (defn block-values [board coord]
-  nil)
+  (set (map (fn [x] (value-at board x)) (block-pairs coord))))
 
 (defn valid-values-for [board coord]
   nil)
