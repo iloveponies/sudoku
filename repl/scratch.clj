8defc37818cdd51865d4e5a19db75f9506dc68a0
Simon Tuohey <simon@tuohey.net>
2014-08-26 19:28:42 +1000

    nearly done - subsets exercise now

--- repl/scratch.clj
+++ repl/scratch.clj
@@ -1,1 +1,79 @@
 (use 'sudoku :reload)
+
+(def sudoku-board
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(def solved-board
+  (board [[5 3 4 6 7 8 9 1 2]
+          [6 7 2 1 9 5 3 4 8]
+          [1 9 8 3 4 2 5 6 7]
+          [8 5 9 7 6 1 4 2 3]
+          [4 2 6 8 5 3 7 9 1]
+          [7 1 3 9 2 4 8 5 6]
+          [9 6 1 5 3 7 2 8 4]
+          [2 8 7 4 1 9 6 3 5]
+          [3 4 5 2 8 6 1 7 9]]))
+
+(def invalid-board
+  (board [[5 3 4 6 7 8 9 1 2]
+          [6 7 2 1 9 5 3 4 8]
+          [1 9 8 3 4 7 5 6 7]
+          [8 5 9 7 6 1 4 2 3]
+          [4 2 6 8 5 3 7 9 1]
+          [7 1 3 9 2 4 8 5 6]
+          [9 6 1 5 3 7 2 8 4]
+          [2 8 7 4 1 9 6 3 5]
+          [3 4 5 2 8 6 1 7 9]]))
+
+(valid-rows? solved-board)  ;=> truthy
+(valid-rows? invalid-board) ;=> falsey
+(valid-cols? solved-board)  ;=> truthy
+(valid-cols? invalid-board) ;=> falsey
+(valid-blocks? solved-board)  ;=> truthy
+(valid-blocks? invalid-board) ;=>
+
+(valid-solution? solved-board)  ;=> truthy
+(valid-solution? invalid-board) ;=> falsey
+
+(set-value-at sudoku-board [1 1] 6)
+(def before-change
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(def after-change
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 4 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(=
+ (set-value-at before-change [2 1] 4)
+ after-change)
+
+(class all-coords)
+(take 10 all-coords)
+(filled? solved-board)
+
+(take 3 (empty-points solved-board))
+(find-empty-point sudoku-board)
+(value-at sudoku-board [0 2])
--- src/sudoku.clj
+++ src/sudoku.clj
@@ -2,57 +2,105 @@
   (:require [clojure.set :as set]))
 
 (def board identity)
+(def board-size 9)
+(def block-size 3)
+(def blocks (/ board-size block-size))
+
+(def all-values (into #{} (range 1 (inc board-size))))
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row _]]
+  (into #{}
+        (for [col (range board-size)]
+          (value-at board [row col]))))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [_ col]]
+  (into #{}
+        (for [row (range board-size)]
+          (value-at board [row col]))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [row coords
+        col coords]
+    [row col]))
 
-(defn block-values [board coord]
-  nil)
+(defn block-start [[row col]]
+  [(* block-size (quot row block-size))
+   (* block-size (quot col block-size))])
+
+(defn block-values [board [row col]]
+  (let [[row-start col-start] (block-start [row col])
+        row-end (+ row-start block-size)
+        col-end (+ col-start block-size)]
+    (into #{}
+          (for [brow (range row-start row-end)
+                bcol (range col-start col-end)]
+            (value-at board [brow bcol])))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if-not (zero? (value-at board coord))
+    #{}
+    (-> all-values
+        (set/difference (row-values board coord))
+        (set/difference (col-values board coord))
+        (set/difference (block-values board coord)))))
+
+(def all-coords
+  (for [row (range board-size)
+        col (range board-size)]
+    [row col]))
+
+(defn all-entries [board]
+    (map #(value-at board %) all-coords))
 
 (defn filled? [board]
-  nil)
+  (not-any?
+   zero?
+   (all-entries board)))
+
+(defn all-valid-houses? [houses]
+  (every? #(= all-values %) houses))
 
 (defn rows [board]
-  nil)
+  (for [row (range board-size)]
+    (row-values board [row 0])))
 
 (defn valid-rows? [board]
-  nil)
+  (all-valid-houses? (rows board)))
 
 (defn cols [board]
-  nil)
+  (for [col (range board-size)]
+    (col-values board [0 col])))
 
 (defn valid-cols? [board]
-  nil)
+  (all-valid-houses? (cols board)))
 
 (defn blocks [board]
-  nil)
+  (for [row [0 3 6]
+        col [0 3 6]]
+    (block-values board [row col])))
 
 (defn valid-blocks? [board]
-  nil)
+  (all-valid-houses? (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board)
+       (valid-cols? board)
+       (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
+
+(defn empty-points [board]
+  (filter #(zero? (value-at board %)) all-coords))
 
 (defn find-empty-point [board]
-  nil)
+  (first (empty-points board)))
 
 (defn solve [board]
   nil)
