139f0269345b93881f4e14412a80627fdb947256
Michel Rasschaert <michel.rasschaert@gmail.com>
2014-10-02 22:26:06 +0200

    ex06

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,23 +3,58 @@
 
 (def board identity)
 
+(def sudoku-board
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(def solved-board
+  (board [[5 3 4 6 7 8 9 1 2]
+          [6 7 2 1 9 5 3 4 8]
+          [1 9 8 3 4 2 5 6 7]
+          [8 5 9 7 6 1 4 2 3]
+          [4 2 6 8 5 3 7 9 1]
+          [7 1 3 9 2 4 8 5 6]
+          [9 6 1 5 3 7 2 8 4]
+          [2 8 7 4 1 9 6 3 5]
+          [3 4 5 2 8 6 1 7 9]]))
+
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not= 0 (value-at board coord)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [r _]]
+  (set (map
+        #(value-at board [r %])
+        (range 9))))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [_ c]]
+  (set (map
+        #(value-at board [% c])
+        (range 9))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [r coords
+        c coords]
+    [r c]))
+
+(defn top-left [[r c]]
+  [(- r (mod r 3)) (- c (mod c 3))])
 
 (defn block-values [board coord]
-  nil)
+  (let [tl (top-left coord)
+        block-coords (map #(map + tl %) (coord-pairs [0 1 2]))]
+    (set (map #(value-at board %) block-coords))))
 
 (defn valid-values-for [board coord]
   nil)
