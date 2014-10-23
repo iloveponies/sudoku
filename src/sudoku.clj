0b73b8cea18dede030f022aff5dfc99be79a09d8
Andreas Andra <aandra@localhost.localdomain>
2014-10-23 16:55:24 +0300

    ei vikaa

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,55 +4,77 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (get-in board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[i j] coord]
+    (set (get board i))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[i j] coord]
+    (set (map (fn [x] (get x j)) board ))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [i coords
+        j coords]
+    [i j]))
+
+(defn block-helper [coord]
+  (let [[i j] coord
+    x (* (int (/ i 3)) 3)
+    y (* (int (/ j 3)) 3)
+    a (range x (+ 3 x))
+        b (range y (+ 3 y))]
+    (for [i a
+          j b]
+      [i j])))
 
 (defn block-values [board coord]
-  nil)
+  (set (map #(value-at board %) (block-helper coord))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+  #{}
+  (set/difference #{1 2 3 4 5 6 7 8 9} (set/union (block-values board coord)
+                                                  (col-values board coord)
+                                                  (row-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (set (apply concat board)) 0)))
 
 (defn rows [board]
-  nil)
+  (map set board))
 
 (defn valid-rows? [board]
-  nil)
+  (apply = #{1 2 3 4 5 6 7 8 9} (rows board)))
 
 (defn cols [board]
-  nil)
+  (map (fn [x] (col-values board [0 x])) (range 9)))
 
 (defn valid-cols? [board]
-  nil)
+  (apply = #{1 2 3 4 5 6 7 8 9} (cols board)))
 
 (defn blocks [board]
-  nil)
+  (for [x [[0 0] [0 3] [0 6] [3 0] [3 3] [3 6] [6 0] [6 3] [6 6]]]
+    (block-values board x)))
 
 (defn valid-blocks? [board]
-  nil)
+  (apply = #{1 2 3 4 5 6 7 8 9} (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-blocks? board) (valid-cols? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (first (filter #(zero? (value-at board %))
+                 (for [x (range 9)
+                       y (range 9)]
+                   [x y]))))
 
 (defn solve [board]
   nil)
