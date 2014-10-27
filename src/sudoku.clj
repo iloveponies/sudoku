c20e797f960633970e9f843a1ec67fc8774d81fa
HeavenlyAwe <christoffer.fridlund@hotmail.com>
2014-10-27 13:16:28 +0200

    Sudoku solver working.

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,30 +4,52 @@
 
 (def all-values #{1 2 3 4 5 6 7 8 9})
 
-(def sudoku-board
-  (board [[5 3 0 0 7 0 0 0 0]
-          [6 0 0 1 9 5 0 0 0]
-          [0 9 8 0 0 0 0 6 0]
-          [8 0 0 0 6 0 0 0 3]
-          [4 0 0 8 0 3 0 0 1]
-          [7 0 0 0 2 0 0 0 6]
-          [0 6 0 0 0 0 2 8 0]
-          [0 0 0 4 1 9 0 0 5]
-          [0 0 0 0 8 0 0 7 9]]))
-
-(def solved-board
-  (board [[5 3 4 6 7 8 9 1 2]
-          [6 7 2 1 9 5 3 4 8]
-          [1 9 8 3 4 2 5 6 7]
-          [8 5 9 7 6 1 4 2 3]
-          [4 2 6 8 5 3 7 9 1]
-          [7 1 3 9 2 4 8 5 6]
-          [9 6 1 5 3 7 2 8 4]
-          [2 8 7 4 1 9 6 3 5]
-          [3 4 5 2 8 6 1 7 9]]))
-
-
 (def board identity)
+
+;(def sudoku-board
+;  (board [[5 3 0 0 7 0 0 0 0]
+;          [6 0 0 1 9 5 0 0 0]
+;          [0 9 8 0 0 0 0 6 0]
+;          [8 0 0 0 6 0 0 0 3]
+;          [4 0 0 8 0 3 0 0 1]
+;          [7 0 0 0 2 0 0 0 6]
+;          [0 6 0 0 0 0 2 8 0]
+;          [0 0 0 4 1 9 0 0 5]
+;          [0 0 0 0 8 0 0 7 9]]))
+;
+;(def solved-board
+;  (board [[5 3 4 6 7 8 9 1 2]
+;          [6 7 2 1 9 5 3 4 8]
+;          [1 9 8 3 4 2 5 6 7]
+;          [8 5 9 7 6 1 4 2 3]
+;          [4 2 6 8 5 3 7 9 1]
+;          [7 1 3 9 2 4 8 5 6]
+;          [9 6 1 5 3 7 2 8 4]
+;          [2 8 7 4 1 9 6 3 5]
+;          [3 4 5 2 8 6 1 7 9]]))
+;
+;(def before-change
+;  (board [[5 3 0 0 7 0 0 0 0]
+;          [6 0 0 1 9 5 0 0 0]
+;          [0 9 8 0 0 0 0 6 0]
+;          [8 0 0 0 6 0 0 0 3]
+;          [4 0 0 8 0 3 0 0 1]
+;          [7 0 0 0 2 0 0 0 6]
+;          [0 6 0 0 0 0 2 8 0]
+;          [0 0 0 4 1 9 0 0 5]
+;          [0 0 0 0 8 0 0 7 9]]))
+;
+;(def after-change
+;  (board [[5 3 0 0 7 0 0 0 0]
+;          [6 0 0 1 9 5 0 0 0]
+;          [0 4 8 0 0 0 0 6 0]
+;          [8 0 0 0 6 0 0 0 3]
+;          [4 0 0 8 0 3 0 0 1]
+;          [7 0 0 0 2 0 0 0 6]
+;          [0 6 0 0 0 0 2 8 0]
+;          [0 0 0 4 1 9 0 0 5]
+;          [0 0 0 0 8 0 0 7 9]]))
+
 
 (defn value-at [board coord]
   (get-in board coord))
@@ -94,19 +116,42 @@
   (every? (fn [col] (valid-set? (col-values board [0 col]))) (range 0 9)))
 
 (defn blocks [board]
-  ()
+  (for [x [0 3 6]
+        y [0 3 6]]
+    (block-values board [x y])))
 
 (defn valid-blocks? [board]
-  nil)
+  (every? (fn [block] (valid-set? block)) (blocks board)))
 
 (defn valid-solution? [board]
   (and (valid-blocks? board) (valid-cols? board) (valid-rows? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (first (filter 
+           (complement nil?)
+           (for [x (range 0 9)
+                 y (range 0 9)]
+             (if (zero? (value-at board [x y]))
+               [x y]
+               nil)))))
 
-(defn solve [board]
-  nil)
+
+(defn solve-helper [cur-board]
+  (if (filled? cur-board)
+    (if (valid-solution? cur-board)
+      [cur-board]
+      [])
+    (let [empty-loc (find-empty-point cur-board)
+          possible-vals (valid-values-for cur-board empty-loc)]
+      (for [val possible-vals
+            solution (let [new-board (set-value-at cur-board empty-loc val)]
+                       (solve-helper new-board))]
+        solution))))
+ (defn solve [board]
+     (solve-helper board))
+
+ 
+ 
