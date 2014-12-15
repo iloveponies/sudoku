299c738d987ede05d5177e5cacc2456e3befb1db
karri <kvrkredd@ymail.com>
2014-12-15 20:25:54 +0530

    :)

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,25 +4,51 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (if (zero? (value-at board coord))
+    false
+    true)
+  )
 
 (defn row-values [board coord]
-  nil)
+  (set (get board (first coord)))
+  )
 
 (defn col-values [board coord]
-  nil)
+  ;; retry with for aye ??
+  (let [col (last coord)]
+    (loop [row 0
+           a-set #{}]
+      (if (= row 9)
+        a-set
+        (recur (inc row)
+               (conj a-set (value-at board (vector row col)))))))
+  )
 
 (defn coord-pairs [coords]
-  nil)
+  (for [row coords
+        col coords]
+    (vector row col))
+  )
 
 (defn block-values [board coord]
-  nil)
+  ;; For a given coordinates, get the set of values in the block this
+  ;; coordinate is part of
+  (let [x-coord (first coord)
+        y-coord (last coord)
+        top-left-x (* (quot x-coord 3) 3)
+        top-left-y (* (quot y-coord 3) 3)
+        block-pairs (for [row (range top-left-x (+ top-left-x 3))
+                          col (range top-left-y (+ top-left-y 3))]
+                      (vector row col))]
+    (reduce #(conj %1 (value-at board %2)) #{} block-pairs))
+  )
 
 (defn valid-values-for [board coord]
-  nil)
+  
+  )
 
 (defn filled? [board]
   nil)
