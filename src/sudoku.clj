c01aa670cd7ef7c79ffdb213323ad8e8e55c3095
user <mohammadharis@gmail.com>
2014-12-20 16:12:29 -0500

    ex 1-14 (solve left)

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,110 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (cond (== 0 (value-at board coord)) false
+        :else true
+  )
+  )
 
 (defn row-values [board coord]
-  nil)
+  (set (get board (get coord 0)))
+  )
 
 (defn col-values [board coord]
-  nil)
+  (set (for [row (range 0 9)]
+    (get-in board (vector row (get coord 1))))
+  )
+)
 
 (defn coord-pairs [coords]
-  nil)
+   (vec (for [x coords y coords] [x y]))
+  )
 
-(defn block-values [board coord]
-  nil)
+(defn top-left [[x y]]
+	 (let [top-x (* (quot x 3) 3)
+	  top-y (* (quot y 3) 3)]
+	 [top-x top-y])
+  )
+
+(defn block-values [board coords]
+
+  (let [x (get (top-left coords) 0) y (get (top-left coords) 1)]
+   (set (for [x (range x (+ x 3))
+         y (range y (+ y 3))]
+          (get-in board [x y]))
+   )
+    )
+  )
 
 (defn valid-values-for [board coord]
-  nil)
-
+  (cond (has-value? board coord) #{}
+      :else (set/difference all-values (row-values board coord) (col-values board coord) (block-values board coord)
+            )
+        )
+  )
+(defn board-values [board]
+  (for [x (range  0 9)
+         y (range  0 9)]
+          (get-in board [x y]))
+  )
 (defn filled? [board]
-  nil)
+  (cond (contains? (set (board-values board)) 0) false
+        :else true
+        )
+  )
 
 (defn rows [board]
-  nil)
+  (into (vector) (map set board))
+  )
 
 (defn valid-rows? [board]
-  nil)
+   (cond  (= (set (rows board)) (into #{} (vector all-values)))   true
+        :else false
+        )
 
+)
 (defn cols [board]
-  nil)
+  (into [] (map set (apply map list board))))
 
 (defn valid-cols? [board]
-  nil)
+    (cond  (= (set (cols board)) (into #{} (vector all-values)))   true
+        :else false
+        )
+
+  )
 
 (defn blocks [board]
-  nil)
+(into (vector)
+      (for [x (range 0 9 3)
+	       y (range 0 9 3)]
+	   (block-values board [x y]))
+      )
+  )
 
 (defn valid-blocks? [board]
-  nil)
+   (cond  (= (set (blocks board)) (into #{} (vector all-values)))   true
+        :else false
+        )
+  )
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board))
+  )
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value)
+  )
 
 (defn find-empty-point [board]
-  nil)
+  (first (filter #(zero? (value-at board %)) (coord-pairs (range 0 9)))
+  )
+)
 
 (defn solve [board]
   nil)
