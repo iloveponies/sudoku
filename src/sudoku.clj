c6a86dcead8c5fbf1167de9eb2fd4f60caadacb3
Verna Koskinen <darth.kipsu@gmail.com>
2014-12-29 21:26:46 +0200

    beginning of sudoku solver

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,29 +3,51 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row _]]
+  (set (get board row)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [_ col]]
+  (set (map (fn [row] (get row col)) board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [row coords
+        col coords]
+    (vector row col)))
+
+(defn coord-pairs-from [[row col]]
+  (let [rows (range row (+ 3 row))
+        cols (range col (+ 3 col))]
+    (for [row rows
+          col cols]
+      (vector row col))))
+
+(defn upper-corner [[row col]]
+  (let [shift (fn [x] (cond
+                        (< x 3) 0
+                        (< x 6) 3
+                        :else 6))]
+    (vector (shift row) (shift col))))
 
 (defn block-values [board coord]
-  nil)
+  (set (map (fn [x] (get-in board x)) (coord-pairs-from (upper-corner coord)))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord) #{}
+    (let [v-block (block-values board coord)
+          v-col (col-values board coord)
+          v-row (row-values board coord)]
+      (set/difference all-values (set/union v-block v-col v-row)))))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (set (apply concat board)) 0)))
 
 (defn rows [board]
   nil)
