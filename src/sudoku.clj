94f95d50b1dc3007d2443f454e014324676d9450
Verna Koskinen <darth.kipsu@gmail.com>
2014-12-30 09:11:06 +0200

    submit sudoku solver

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -50,31 +50,50 @@
   (not (contains? (set (apply concat board)) 0)))
 
 (defn rows [board]
-  nil)
+  (map set board))
+
+(defn valid? [pred]
+  (reduce (fn [acc n] (and acc (= n all-values))) true pred))
 
 (defn valid-rows? [board]
-  nil)
+  (valid? (rows board)))
 
 (defn cols [board]
-  nil)
+  (map (fn [x] (col-values board [0 (dec x)])) all-values))
 
 (defn valid-cols? [board]
-  nil)
+  (valid? (cols board)))
 
 (defn blocks [board]
-  nil)
+  (map (fn [x] (block-values board x)) (coord-pairs [0 3 6])))
 
 (defn valid-blocks? [board]
-  nil)
+  (valid? (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (loop [row 0
+         col 0]
+    (if (== 0 (value-at board [row col]))
+      [row col]
+      (recur (if (== col 8) (inc row) row)
+             (if (== col 8) 0 (inc col))))))
+
+(defn solve-helper [board]
+  (if (filled? board)
+    (if (valid-solution? board)
+      [board]
+      [])
+    (let [point (find-empty-point board)
+          values (valid-values-for board point)]
+          (for [value values
+                solution (solve-helper (set-value-at board point value))]
+            solution))))
 
 (defn solve [board]
-  nil)
+  (first (solve-helper board)))
