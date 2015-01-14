f670774c8895332394c8504da03353f9a3cf1162
Paulius Šarka <paulius.sarka@gmail.com>
2015-01-14 23:30:05 +0100

    done

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -2,57 +2,122 @@
   (:require [clojure.set :as set]))
 
 (def board identity)
+(def id identity)
+
+(def all-values #{1 2 3 4 5 6 7 8 9})
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not= 0 (value-at board coord)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [r c]]
+  (set (get board r)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [r c]]
+  (set (map (fn [row] (get row c)) board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [r coords
+        c coords]
+    [r c]))
 
-(defn block-values [board coord]
-  nil)
+(defn d [x]
+  (cond (<= x 2) 0
+        (<= x 5) 3
+        :else 6 ))
+
+(defn block-values [board [x y]]
+  (set (apply concat (map (fn [row] (take 3 (drop (d y) row))) 
+                    (take 3 (drop (d x) board))))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values (set/union 
+                                 (row-values board coord)
+                                 (col-values board coord)
+                                 (block-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (every? id (map (fn [row] (not-any? zero? row)) board)))
 
 (defn rows [board]
-  nil)
+  (map set board))
+
+(defn d? [list] (apply distinct? (remove zero? list)))
 
 (defn valid-rows? [board]
-  nil)
+  (every? id (map d? board)))
 
 (defn cols [board]
-  nil)
+  (map set (apply map vector board)))
 
 (defn valid-cols? [board]
-  nil)
+  (every? id (map d? (apply map vector board))))
+
+(defn szip [as bs]
+    (loop [sa-seq as
+                    b-seq bs
+                    have []]
+          (if (or (empty? sa-seq) (empty? b-seq))
+                  have
+                  (recur (rest sa-seq)
+                                      (rest b-seq)
+                                      (conj have (conj (first sa-seq) (first b-seq)))))))
+
+(defn encaps [as]
+    (reduce (fn [have i] (conj have [i])) [] as))
+
+(defn zip [a-seq & more]
+    (reduce szip (encaps a-seq) more))
+
+(defn azip [list] (apply zip list))
+
+(defn strands [board]
+  (apply concat (map azip [(take 3 board) (take 3 (drop 3 board)) (drop 6 board)])))
+
+(defn li-blocks [board]
+  (loop [trp (strands board)
+         have []]
+    (if (empty? trp)
+      have
+      (recur (drop 3 trp) 
+             (conj have (apply concat (take 3 trp)))))))
 
 (defn blocks [board]
-  nil)
-
+  (map set (li-blocks board)))
+       
 (defn valid-blocks? [board]
-  nil)
+  (every? id (map d? (li-blocks board))))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board)
+       (valid-cols? board)
+       (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
-(defn find-empty-point [board]
-  nil)
+(defn find-empty-point [var-board]
+  (loop [board var-board
+         row 0]
+    (if (empty? board)
+      nil
+      (let [col (.indexOf (first board) 0)]
+        (if (not= -1 col)
+          [row col]
+          (recur (rest board) (inc row)))))))
+
+
+(defn solve-helper [board]
+  (if (filled? board)
+    [board]
+    (let [emp (find-empty-point board)]
+      (for [nbr (valid-values-for board emp)
+            brd (solve-helper (set-value-at board emp nbr))]
+        brd))))
 
 (defn solve [board]
-  nil)
+  (first (solve-helper board)))
