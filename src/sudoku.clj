2dd623ff7f4475be134c98b3df35db912803f07f
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2015-01-20 21:07:59 +0200

    First part of sudoku

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -2,30 +2,48 @@
   (:require [clojure.set :as set]))
 
 (def board identity)
+(def all-values #{1 2 3 4 5 6 7 8 9})
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not= (value-at board coord) 0))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [r c]]
+  (set (get board r)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [r c]]
+  (set (map (fn [a-seq] (get a-seq c)) board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [r coords
+        c coords]
+    [r c]))
+
+(defn top-left [[r c]]
+  [(- r (mod r 3)) (- c (mod c 3))])
 
 (defn block-values [board coord]
-  nil)
+  (let [[r c] (top-left coord)
+        index-pairs (coord-pairs [0 1 2])
+        values (for [[cr cc] index-pairs]
+                 (value-at board [(+ r cr) (+ c cc)]))]
+    (set values)))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (not= (value-at board coord) 0)
+    #{}
+    (let [used-values (set/union (row-values board coord)
+                                 (col-values board coord)
+                                 (block-values board coord))]
+      (set/difference all-values used-values))))
+
+(defn board-values [board]
+  (set (apply concat board)))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (board-values board) 0)))
 
 (defn rows [board]
   nil)
