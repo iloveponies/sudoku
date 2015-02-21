4314c521216663737df2b94a49bc650cd5e8a4e0
Pete <pmmakinen@hotmail.com>
2015-02-21 15:26:31 +0200

    all but 14 and 15 done

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -45,34 +45,80 @@
       :else (recur (conj ret-set (value-at board [curY curX])) (inc curY) curX)))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (not= (value-at board coord) 0)
+    #{}
+    (clojure.set/difference all-values (clojure.set/union (col-values board coord) (row-values board coord) (block-values board coord)))))
+
+(defn all-in-board [board]
+  (loop [ret-set #{}
+         cur-col 0]
+    (cond
+     (= cur-col 9) ret-set
+     :else (recur (clojure.set/union ret-set (col-values board [0 cur-col])) (inc cur-col))
+    )))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (all-in-board board) 0)))
 
 (defn rows [board]
-  nil)
+    (loop [ret-seq '()
+         cur-row 0]
+    (cond
+     (= cur-row 9) (reverse ret-seq)
+     :else (recur (conj ret-seq (row-values board [cur-row 0])) (inc cur-row))
+    )))
+
+(defn set-valid? [row-set]
+  (loop [row row-set
+         all all-values]
+    (cond
+     (empty? all) true
+     (not (contains? row (first all))) false
+     :else (recur row (rest all)))))
 
 (defn valid-rows? [board]
-  nil)
+  (loop [rows (rows board)]
+    (cond
+     (empty? rows) true
+     (not (set-valid? (first rows))) false
+     :else (recur (rest rows)))))
 
 (defn cols [board]
-  nil)
+    (loop [ret-seq '()
+         cur-col 0]
+    (cond
+     (= cur-col 9) (reverse ret-seq)
+     :else (recur (conj ret-seq (col-values board [0 cur-col])) (inc cur-col))
+    )))
 
 (defn valid-cols? [board]
-  nil)
+  (loop [cols (cols board)]
+    (cond
+     (empty? cols) true
+     (not (set-valid? (first cols))) false
+     :else (recur (rest cols)))))
 
 (defn blocks [board]
-  nil)
+  (loop [ret-seq '()
+         curY 0
+         curX 0]
+    (cond
+     (and (= curY 6) (= curX 6)) (reverse (conj ret-seq (block-values board [curY curX])))
+     (= curX 6) (recur (conj ret-seq (block-values board [curY curX])) (+ curY 3) 0)
+     :else (recur (conj ret-seq (block-values board [curY curX])) curY (+ curX 3)))))
 
 (defn valid-blocks? [board]
-  nil)
+  (loop [blocks (blocks board)]
+    (cond
+     (empty? blocks) true
+     (not (set-valid? (first blocks))) false
+     :else (recur (rest blocks)))))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
   nil)
