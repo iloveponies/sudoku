763068f2931fb13510b27b348d4cd9ed0e3b50d8
Theo Reyes <vincintz@yahoo.com>
2015-02-22 16:34:42 +0800

    block-values

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -13,8 +13,9 @@
   (set (get board row)))
 
 (defn col-values [board [_ col]]
-  (let [col-vect (for [row (range 9)]
-                   (value-at board [row col]))]
+  (let [col-vect
+        (for [row (range 9)]
+          (value-at board [row col]))]
     (set col-vect)))
 
 (defn coord-pairs [coords]
@@ -22,8 +23,18 @@
         col coords]
     [row col]))
 
-(defn block-values [board coord]
-  nil)
+(defn top-left [n]
+  (* (quot n 3) 3))
+
+(defn block-values [board [row col]]
+  (let [top    (top-left row)
+        left   (top-left col)
+        rows   (range top (+ top 3))
+        cols   (range left (+ left 3))
+        coords (for [r rows
+                     c cols]
+                 (value-at board [r c]))]
+    (set  coords)))
 
 (defn valid-values-for [board coord]
   nil)
