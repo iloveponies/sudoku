d9a0fd911702097718d4d0e41d969e9d090be25f
prodprod <jutsopp@gmail.com>
2015-03-07 05:24:30 +0200

    everything besides solve done

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,83 @@
 
 (def board identity)
 
+(def all-values ( set[1 2 3 4 5 6 7 8 9]))
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (if (= (value-at board coord) 0) false true))
 
 (defn row-values [board coord]
-  nil)
+  (let [y (first coord)]
+    (set (get board y))))
 
 (defn col-values [board coord]
-  nil)
+  (let [x (second coord)
+        gety (fn[a, row] (conj a (get row x)))]
+   (set (reduce gety () board) )))
 
 (defn coord-pairs [coords]
-  nil)
+  (vec (for [x coords
+        y coords]
+      [x y])))
 
 (defn block-values [board coord]
-  nil)
+  (let [tly (int (- (first coord) (mod (first coord) 3)))
+        tlx (int (- (second coord) (mod (second coord) 3)) )]
+   (set (for [y (range tly (+ tly 3))
+          x (range tlx (+ tlx 3))]
+      (value-at board [y x]))
+    )))
 
 (defn valid-values-for [board coord]
-  nil)
+  (let [row (row-values board coord)
+        col (col-values board coord)
+        block (block-values board coord)]
+  (if (has-value? board coord) (set ())
+    (set/difference all-values (set/union row (set/union col block))))))
 
 (defn filled? [board]
-  nil)
+ (if (contains? (set (for [y [0 1 2 3 4 5 6 7 8]
+    x [0 1 2 3 4 5 6 7 8]]
+    (value-at board [y x]))) 0)
+   false
+   true))
 
 (defn rows [board]
-  nil)
+  (for [y [0 1 2 3 4 5 6 7 8]]
+    (row-values board [y 0 ])))
 
 (defn valid-rows? [board]
-  nil)
+  (if (empty? (disj (set (rows board)) all-values)) true false))
 
 (defn cols [board]
-  nil)
+  (for [x [0 1 2 3 4 5 6 7 8]]
+    (col-values board [0 x])))
 
 (defn valid-cols? [board]
-  nil)
+  (if (empty? (disj (set (cols board)) all-values)) true false))
 
 (defn blocks [board]
-  nil)
+  (for [x [0 3 6]
+        y [0 3 6]]
+    (block-values board [x y])))
 
 (defn valid-blocks? [board]
-  nil)
+  (if (empty? (disj (set (blocks board)) all-values)) true false))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-blocks? board) (valid-cols? board) (valid-rows? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
-
+  (loop [seq (coord-pairs all-values)]
+    (if (empty? seq) nil
+      (if ((complement has-value?) board (first seq))
+        (first seq)
+        (recur (rest seq))))))
 (defn solve [board]
   nil)
