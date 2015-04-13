e1e7a64d59c8e707a7bb58d607d30cfcc6ebfbad
Markus Mattila <markus_mattila@hotmail.com>
2015-04-13 10:32:17 +0300

    finished

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,92 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not(== (value-at board coord) 0)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row -]]
+  (set(get board row)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [- col]]
+  (set(map (fn[row] (get row col)) board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [x coords
+        y coords]
+    [x y]))
+
+(defn block-coords [[x y]]
+  (let [[top-x top-y] [(- x (mod x 3))(- y (mod y 3))]
+        x-values [top-x (+ top-x 1) (+ top-x 2)]
+        y-values [top-y (+ top-y 1) (+ top-y 2)]]
+    (for [a x-values
+          b y-values]
+      [a b])))
 
 (defn block-values [board coord]
-  nil)
+  (set(map (fn[c] (value-at board c)) (block-coords coord))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference  all-values
+                     (set/union (block-values board coord)
+                                (row-values board coord)
+                                (col-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (let [rows (for [row [0 1 2 3 4 5 6 7 8]](row-values board [row 0]))]
+  (every?  (fn[x](= x false)) (map (fn[x] (contains? x 0)) rows))))
 
 (defn rows [board]
-  nil)
+  (for [row [0 1 2 3 4 5 6 7 8]] (row-values board [row 0])))
+
+
+(defn validator [elem]
+  (and (not(contains? elem 0)) (== (count elem) 9)))
 
 (defn valid-rows? [board]
-  nil)
+   (every?  (fn[x](= x true)) (map validator (rows board))))
 
 (defn cols [board]
-  nil)
+  (for [col [0 1 2 3 4 5 6 7 8]](col-values board [0 col])))
 
 (defn valid-cols? [board]
-  nil)
+   (every?  (fn[x](= x true)) (map validator (cols board))))
 
 (defn blocks [board]
-  nil)
+  (let [block-coords (for [x [0 3 6]
+                           y [0 3 6]] [x y])]
+   (for [block block-coords] (block-values board block))))
 
 (defn valid-blocks? [board]
-  nil)
+   (every?  (fn[x](= x true)) (map validator (blocks board))))
+
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (let [all-coords (for [x [0 1 2 3 4 5 6 7 8]
+                         y [0 1 2 3 4 5 6 7 8]] [x y])]
+    (first(filter (complement (fn[coord] (has-value? board coord))) all-coords))))
+
 
 (defn solve [board]
-  nil)
+  (if (filled? board)
+    (if (valid-solution? board)
+      board
+      ())
+    (let [point (find-empty-point board)
+          valid-values (valid-values-for board point)]
+      (first (filter (complement empty?)
+                     (for [value valid-values]
+                       (solve (set-value-at board point value))))))))
