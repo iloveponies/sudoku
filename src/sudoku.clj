917ae58351fc64a28009b56bb4f1539a6ddaf4b5
asena01 <dawit.weldemeskel@gmail.com>
2015-04-16 17:06:40 +0300

    lastExercise

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,43 +4,60 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[r _] coord](set (get board r))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[_ c] coord]
+  (set (map (fn [x] (get x c)) board))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [m coords n coords][m n]))
 
 (defn block-values [board coord]
-  nil)
+  (let [[r c] coord
+   tlc-m (* (quot r 3) 3)
+   tlc-n (* (quot c 3) 3)
+   br-m (vec (range tlc-m (+ tlc-m 3)))
+   br-n (vec (range tlc-n (+ tlc-n 3)))]
+  (set (for [m br-m
+             n br-n]
+  (get-in board [m n])))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (let [v (set (range 1 10))
+   rv (row-values board coord)
+   cv (col-values board coord)
+   bv (block-values board coord)
+   rsv (set/union rv cv bv)]
+  (if (has-value? board coord)
+   #{}(set/difference v rsv))))
 
 (defn filled? [board]
-  nil)
+  (not (some zero? (flatten board))))
 
 (defn rows [board]
-  nil)
+  (map #(row-values board [% 0])
+  (range 0 9)))
 
 (defn valid-rows? [board]
   nil)
 
 (defn cols [board]
-  nil)
+   (map #(col-values board [0 %])
+   (range 0 9)))
 
 (defn valid-cols? [board]
   nil)
 
 (defn blocks [board]
-  nil)
+  (map #(block-values board %)
+  (coord-pairs [0 3 6])))
 
 (defn valid-blocks? [board]
   nil)
@@ -49,10 +66,11 @@
   nil)
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (first (filter #(zero? (value-at board %))
+       (coord-pairs (range 0 9)))))
 
 (defn solve [board]
   nil)
