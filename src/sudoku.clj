2549fccac4c0345955efbb729c442c45d4d7541a
Khalid Alnajjar <alnajjar@cs.helsinki.fi>
2015-04-27 20:01:41 -0700

    message

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,53 +3,66 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (contains? all-values (value-at board coord)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row]]
+  (set (get board row)))
 
 (defn col-values [board coord]
-  nil)
+  (let [[x y] coord]
+    (row-values (apply mapv vector board) [y x])))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [x coords y coords] [x y]))
+
+(defn top-left [coords]
+  (map (fn [x] (* 3 (int (/ x 3))) ) coords))
 
 (defn block-values [board coord]
-  nil)
+  (set (let [[x y] (top-left coord)]
+    (for [i [0 1 2] j [0 1 2]]
+      (value-at board [(+ i x) (+ j y)] )))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord) #{}
+    (set/difference all-values (set/union
+                                (col-values board coord)
+                                (block-values board coord)
+                                (row-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (set (flatten board)) 0)))
 
 (defn rows [board]
-  nil)
+  (map (fn [x] (set x)) board))
 
 (defn valid-rows? [board]
-  nil)
+  (every? #(empty? (set/difference all-values %)) (rows board)))
 
 (defn cols [board]
-  nil)
+  (rows (apply mapv vector board)))
 
 (defn valid-cols? [board]
-  nil)
+  (valid-rows? (apply mapv vector board)))
 
 (defn blocks [board]
-  nil)
+  (for [row [0 3 6] col [0 3 6]]
+    (block-values board [row col])))
 
 (defn valid-blocks? [board]
-  nil)
+  (every? #(empty? (set/difference all-values %)) (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-blocks? board) (valid-rows? board) (valid-cols? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
   nil)
