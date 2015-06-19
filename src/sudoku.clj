b2a196f006ddc5956128c4901bc97b0857a77bab
Eng <engd@SEA-ENGD-M.local>
2015-06-18 19:34:53 -0700

    up to step 6

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -1,26 +1,53 @@
 (ns sudoku
   (:require [clojure.set :as set]))
+(apply mapv vector sudoku-board)
 
+(apply + [1 2 3 4])
+(def sudoku-board
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (= 0 (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
-
+  (let [[row col] coord]
+    (set (get board row))))
+(defn transpose[board]
+  (apply mapv vector board))
 (defn col-values [board coord]
-  nil)
+  (let [[row col] coord]
+    (set (get (transpose board) col))))
+(coord-pairs [0 1 2])
+(defn coord-pairs 
+  ([coords] (for [p coords
+        q coords]
+    [p q]))
+  ([coord1 coord2] (for [p coord1
+                        q coord2]
+                    [p q])))
 
-(defn coord-pairs [coords]
-  nil)
-
+(block-values sudoku-board [8 8])
 (defn block-values [board coord]
-  nil)
-
+  (let [bordcoord (fn[section] (range (* section 3) (* (inc section) 3)))
+        sect (fn[coord] (int (/ coord 3)))
+        [row col] coord
+        rowcords (bordcoord (sect row))
+        colcords (bordcoord (sect col))]
+    (reduce #(conj %1 (value-at board %2)) #{} (coord-pairs rowcords colcords))
+    ))
+(block-values sudoku-board [0 2])
 (defn valid-values-for [board coord]
   nil)
 
