fd8f0b793e1af7521ce0bc0e2791c28af7928edc
Hans Liljestrand <ishkamiel@gmail.com>
2015-11-02 16:31:24 +0200

    almost there

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -31,46 +31,107 @@
         y coords]
     (vec [x y])))
 
-(defn craptacular [a b] 
-  (vec [(* (unchecked-divide-int a 3) 3)
-        (* (unchecked-divide-int b 3) 3)]
+(defn top-left [[row col]] 
+  (vec [(* (unchecked-divide-int row 3) 3)
+        (* (unchecked-divide-int col 3) 3)]
        ))
 
 (defn block-values [board coord]
-  nil)
+  (let [[row col] (top-left coord)]
+    (set (for [i (range 0 3)
+               j (range 0 3)]
+           (value-at board [(+ row i) (+ col j)])
+           ))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (let [numbers (set (range 1 10))]
+    (if (> (value-at board coord) 0) 
+      #{}
+      (clojure.set/difference numbers
+                              (block-values board coord)
+                              (row-values board coord)
+                              (col-values board coord)
+                              ))))
 
-(defn filled? [board]
-  nil)
+;; Moved this to use find-empty-point (minimize code copy-paste)
+;; (defn filled? [board]
+;;   (loop [i 0]
+;;     (let [row (unchecked-divide-int i 9) 
+;;           col (mod i 9)]
+;;       (cond 
+;;         (= 0 (value-at board [row col])) false
+;;         (= row 9) true
+;;         :else (recur (inc i))
+;;         ))))
 
 (defn rows [board]
-  nil)
-
-(defn valid-rows? [board]
-  nil)
+  (for [row (range 0 9)]
+    (row-values board [row 0])))
 
 (defn cols [board]
-  nil)
-
-(defn valid-cols? [board]
-  nil)
+  (for [col (range 0 9)]
+    (col-values board [0 col])))
 
 (defn blocks [board]
-  nil)
+  (for [row (range 0 3)
+        col (range 0 3)]
+    (block-values board [(* row 3) (* col 3)])))
+
+(defn has-all-values? [values]
+  (empty? (clojure.set/difference (set (range 1 10)) values)))
+
+(defn valid-rows? [board]
+  (every? has-all-values? (rows board)))
+
+(defn valid-cols? [board]
+  (every? has-all-values? (cols board)))
 
 (defn valid-blocks? [board]
-  nil)
+  (every? has-all-values? (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-blocks? board)
+       (valid-rows? board)
+       (valid-cols? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (loop [i 0]
+    (let [row (unchecked-divide-int i 9) 
+          col (mod i 9)]
+      (cond 
+        (= 0 (value-at board [row col])) [row col]
+        (= row 9) nil
+        :else (recur (inc i))
+        ))))
+
+(defn filled? [board] (if (find-empty-point board) false true))
 
 (defn solve [board]
-  nil)
+  (if (valid-solution? board) 
+    board
+    (let [empty-point (find-empty-point board)]
+      (if (empty? empty-point)
+        #{}
+        (loop [value 1] 
+          (let [solution (solve (set-value-at board empty-point value))]
+            (cond 
+              (not-empty solution) solution
+              (= value 9) #{}
+              :else (recur (inc value)))))))))
+
+
+;; (defn solve [board]
+;;   (let [empty-point (find-empty-point board)]
+;;     (cond
+;;       (not-empty empty-point) (loop [value 1]
+;;                                 (let [solution (solve (set-value-at board empty-point value))]
+;;                                   (cond 
+;;                                     (not-empty solution) solution
+;;                                     (> value 9) nil
+;;                                     :else (recur (inc value)))))
+;;       (valid-solution? board) board
+;;       :else #{})))
+
