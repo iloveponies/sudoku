c4cdb712d02bad86b5d0ac2f1c6f519b2ec77e65
aam <aam@anahoret.com>
2015-12-29 18:53:54 +0200

    Implement tasks 1-6

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,23 +3,53 @@
 
 (def board identity)
 
+(def sudoku-board
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not= 0 (value-at board coord)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row col]]
+  (set (get board row)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [row col]]
+  (loop [r 0
+         values #{}]
+    (if (== r 9)
+      values
+      (recur
+       (inc r)
+       (conj values (value-at board [r col]))))))
 
-(defn coord-pairs [coords]
-  nil)
+(defn coord-pairs [coord-sequence]
+  (for [r coord-sequence
+        c coord-sequence]
+    [r c]))
 
-(defn block-values [board coord]
-  nil)
+(defn block-values [board [x y]]
+  (def shifted-coords
+    (fn [[shif-top shift-left]]
+      (map (fn [[row col]] [(+ row shif-top) (+ col shift-left)])
+           (coord-pairs [3 4 5]))))
+
+  (let [[top left] [(* (quot x 3) 3) (* (quot y 3) 3)]
+        shifts [(- top 3) (- left 3)]
+        real-coords (shifted-coords shifts)]
+    (set
+     (map
+      (fn [coord] (value-at board coord))
+      real-coords))))
 
 (defn valid-values-for [board coord]
   nil)
