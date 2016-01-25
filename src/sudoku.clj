0b2395855d77fb0212296582e53ab9c2b9ab24ec
Sami Ketola <sami.ketola@kapsi.fi>
2016-01-25 16:40:34 +0200

    Made first part of these while attending a useless meeting

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,23 +3,59 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
+(def sudoku-board
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(def solved-board
+  (board [[5 3 4 6 7 8 9 1 2]
+          [6 7 2 1 9 5 3 4 8]
+          [1 9 8 3 4 2 5 6 7]
+          [8 5 9 7 6 1 4 2 3]
+          [4 2 6 8 5 3 7 9 1]
+          [7 1 3 9 2 4 8 5 6]
+          [9 6 1 5 3 7 2 8 4]
+          [2 8 7 4 1 9 6 3 5]
+          [3 4 5 2 8 6 1 7 9]]))
+
 (defn value-at [board coord]
-  nil)
+  (nth (nth board (first coord)) (second coord)))
 
 (defn has-value? [board coord]
-  nil)
+  (if (not (= 0 (value-at board coord)))
+    true
+    false))
 
 (defn row-values [board coord]
-  nil)
+  (set (nth board (first coord))))
 
 (defn col-values [board coord]
-  nil)
+  (set 
+    (for [x (range 0 9)]
+    (value-at board [x (second coord)]))))
 
 (defn coord-pairs [coords]
-  nil)
+  (vec
+  (for [x coords
+        y coords]
+    (apply vector [x y]))))
+
+(defn block-coordinates [coord]
+  (let [offset [(*(int (/ (first coord) 3)) 3) (*(int (/ (second coord) 3)) 3)]]
+        (vec (map (fn [pos] (vec (map + pos offset))) (coord-pairs [0 1 2])))))
 
 (defn block-values [board coord]
-  nil)
+  (set
+    (map (fn [pos] (value-at board pos)) (block-coordinates coord))))
 
 (defn valid-values-for [board coord]
   nil)
