b11204507a87796af80953895f0b6c30d8b991ef
Sami Ketola <sami.ketola@kapsi.fi>
2016-02-25 19:29:10 +0200

    Did the rest of the Sudoku excersises. I think I'm done here.

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -5,28 +5,6 @@
 
 (def all-values #{1 2 3 4 5 6 7 8 9})
 
-(def sudoku-board
-  (board [[5 3 0 0 7 0 0 0 0]
-          [6 0 0 1 9 5 0 0 0]
-          [0 9 8 0 0 0 0 6 0]
-          [8 0 0 0 6 0 0 0 3]
-          [4 0 0 8 0 3 0 0 1]
-          [7 0 0 0 2 0 0 0 6]
-          [0 6 0 0 0 0 2 8 0]
-          [0 0 0 4 1 9 0 0 5]
-          [0 0 0 0 8 0 0 7 9]]))
-
-(def solved-board
-  (board [[5 3 4 6 7 8 9 1 2]
-          [6 7 2 1 9 5 3 4 8]
-          [1 9 8 3 4 2 5 6 7]
-          [8 5 9 7 6 1 4 2 3]
-          [4 2 6 8 5 3 7 9 1]
-          [7 1 3 9 2 4 8 5 6]
-          [9 6 1 5 3 7 2 8 4]
-          [2 8 7 4 1 9 6 3 5]
-          [3 4 5 2 8 6 1 7 9]]))
-
 (defn value-at [board coord]
   (nth (nth board (first coord)) (second coord)))
 
@@ -58,7 +36,12 @@
     (map (fn [pos] (value-at board pos)) (block-coordinates coord))))
 
 (defn valid-values-for [board coord]
-  nil)
+(if (has-value? board coord)
+  #{}
+  (set/difference all-values
+                  (col-values board coord)
+                  (row-values board coord)
+                  (block-values board coord))))
 
 (defn filled? [board]
   (not (some (fn [row] (contains? (set row) 0)) board)))
@@ -67,28 +50,40 @@
   (vec (map set board)))
 
 (defn valid-rows? [board]
-  nil)
+  (every? (fn [row] (= all-values row)) (rows board)))
 
 (defn cols [board]
-  nil)
+  (for [x (range 0 9)] (col-values board [0 x])))
 
 (defn valid-cols? [board]
-  nil)
+  (every? (fn [col] (= all-values col)) (cols board)))
 
 (defn blocks [board]
-  nil)
+  (for [x (range 0 3)
+        y (range 0 3)]
+    (block-values board [(* 3 x) (* 3 y)])))
 
 (defn valid-blocks? [board]
-  nil)
+  (every? (fn [block] (= all-values block)) (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (first (filter (complement nil?)
+                 (for [x (range 0 9)
+                       y (range 0 9)]
+                   (if (not (has-value? board [x y]))
+                     [x y])))))
 
 (defn solve [board]
-  nil)
+  (if (valid-solution? board)
+    board
+    (let [ emptyCoord (find-empty-point board) ] 
+      (for [step (valid-values-for board emptyCoord)
+            nextStep (solve (set-value-at board emptyCoord step))]
+        nextStep))))
+
