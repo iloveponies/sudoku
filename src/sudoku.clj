bb99b7799182a0c61e69becf7f7d8b2c355ab5fd
Tuukka Paukkunen <tuukka.paukkunen@cs.helsinki.fi>
2016-05-02 17:03:03 +0300

    Done until ex 7

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,26 +3,44 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+ (not (zero? (get-in board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[row _] coord]
+    (set (get board row))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[_ col] coord]
+    (set (map (fn [x] (value-at board [x col])) (range 9)))))
 
 (defn coord-pairs [coords]
-  nil)
+  (apply vector (for [row coords
+                      col coords]
+                  [row col])))
+
+(defn block-values-helper [coord]
+  (let [[x y] coord]
+    [(* (quot x 3) 3) (* (quot y 3) 3)]))
 
 (defn block-values [board coord]
-  nil)
+  (let [[start-r start-c] (block-values-helper coord)]
+    (set (for [row (range start-r (+ 3 start-r))
+               col (range start-c (+ 3 start-c))]
+           (value-at board [row col])))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values
+                    (row-values board coord)
+                    (col-values board coord)
+                    (block-values board coord))))
 
 (defn filled? [board]
   nil)
