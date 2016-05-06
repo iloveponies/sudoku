c646fc0912150c278f15e587c01e5d00fb13a389
Xendras <jonas.westerlund@helsinki.fi>
2016-05-06 23:56:40 +0300

    sudoku done, except solver

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -23,13 +23,15 @@
   (for [first coords second coords]
     [first second]))
 
-(defn block-helper [coords]
-  (let [[x y] coords]
+(defn block-helper [coord]
+  (let [[x y] coord]
   [(- x (mod x 3)) (- y (mod y 3))]))
 
 (defn block-values [board coord]
   (let [[x y] (block-helper coord)]
-    (set (for [coordinate (coord-pairs [x (+ x 1) (+ x 2)])]
+    (set (for [coordinate [[x y] [(+ x 1) y] [(+ x 2) y]
+                           [x (+ y 1)] [(+ x 1) (+ y 1)] [(+ x 2) (+ y 1)]
+                           [x (+ y 2)] [(+ x 1) (+ y 2)] [(+ x 2) (+ y 2)]]]
        (value-at board coordinate)))))
 
 (defn valid-values-for [board coord]
@@ -40,36 +42,54 @@
                     (col-values board coord))
     #{}))
 
+(defn fill-helper [board]
+  (for [coord (coord-pairs [0 1 2 3 4 5 6 7 8])]
+    (value-at board coord)))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (set (fill-helper board)) 0)))
 
 (defn rows [board]
-  nil)
+  (for [number #{0 1 2 3 4 5 6 7 8}]
+    (set (row-values board [number 0]))))
 
 (defn valid-rows? [board]
-  nil)
+  (not (contains? (set (for [row (rows board)]
+                   (and (== (count row) 9) (not (contains? row 0)))))
+                  false)))
 
 (defn cols [board]
-  nil)
+  (for [number #{0 1 2 3 4 5 6 7 8}]
+    (set (col-values board [0 number]))))
 
 (defn valid-cols? [board]
-  nil)
+  (not (contains? (set (for [col (cols board)]
+                   (and (== (count col) 9) (not (contains? col 0)))))
+                  false)))
 
 (defn blocks [board]
-  nil)
+  (for [coords [[0 0] [0 3] [0 6] [3 0] [3 3] [3 6] [6 0] [6 3] [6 6]]]
+    (set (block-values board coords))))
 
 (defn valid-blocks? [board]
-  nil)
+  (not (contains? (set (for [block (blocks board)]
+                   (and (== (count block) 9) (not (contains? block 0)))))
+                  false)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
+
+(defn find-empty-helper [board]
+  (set (for [coords (coord-pairs [0 1 2 3 4 5 6 7 8])]
+    (if (zero? (value-at board coords))
+      coords
+      nil))))
 
 (defn find-empty-point [board]
-  nil)
+  (first (set/difference (find-empty-helper board) #{nil})))
 
 (defn solve [board]
   nil)
