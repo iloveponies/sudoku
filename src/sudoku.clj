1473d8487c17bfa6e786db3a5c77e5a70f8879d2
Guo Qiang <gqxuziling@yahoo.com>
2016-07-04 11:34:16 +0800

    submit 1

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,22 +4,34 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (== 0 (get-in board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[x y] coord]
+    (set (get board x))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[x y] coord]
+    (set (map (fn [elem] (get elem y)) board))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [x  coords
+        y  coords]
+      (concat [x y] )))
+
+(defn top-left [coords]
+  (let [[x y] coords]
+    [(- x (mod x 3)) (- y (mod y 3))])  
+  )
 
 (defn block-values [board coord]
-  nil)
+  (set (let [[topx topy] (top-left coord)]
+    (for [x (range topx (+ topx 3))
+          y (range topy (+ topy 3))]
+      (cons (get-in board [x y])  nil)))))
 
 (defn valid-values-for [board coord]
   nil)
