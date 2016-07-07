e4046f41746a9c3fbccdd57a2122d0e792010dce
Guo Qiang <gqxuziling@yahoo.com>
2016-07-07 14:52:41 +0800

    done

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -28,43 +28,83 @@
   )
 
 (defn block-values [board coord]
-  (set (let [[topx topy] (top-left coord)]
-    (for [x (range topx (+ topx 3))
-          y (range topy (+ topy 3))]
-      (cons (get-in board [x y])  nil)))))
+  (set (let [[topx topy] (top-left coord)
+             result '()]
+    (set (map first(for [x (range topx (+ topx 3))
+                         y (range topy (+ topy 3))]
+                     (cons (get-in board [x y])  nil)))))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference 
+      (set/difference 
+        (set/difference all-values (block-values board coord))
+        (row-values board coord))
+      (col-values board coord))))
 
 (defn filled? [board]
-  nil)
+  (let [values 
+        (apply set/union (for [x (range 0 9)]
+                           (set/union (row-values board [x 0]))))
+        ]
+    (not (contains? values 0))))
 
 (defn rows [board]
-  nil)
+  (for [x (range 0 9)]
+      (set/union (row-values board [x 0]))))
+
+(defn valid? [a-set]
+  (= a-set all-values))
 
 (defn valid-rows? [board]
-  nil)
+  (every? true? (map valid? (rows board))))
 
 (defn cols [board]
-  nil)
+   (for [x (range 0 9)]
+      (set/union (col-values board [0 x]))))
 
 (defn valid-cols? [board]
-  nil)
+  (every? true? (map valid? (cols board))))
 
 (defn blocks [board]
-  nil)
+  (for [x [0 3 6]
+        y [0 3 6]]
+      (set/union (block-values board [x y]))))
 
 (defn valid-blocks? [board]
-  nil)
+  (every? true? (map valid? (blocks board))))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (first (disj (set (for [x (range 0 9)
+                   y (range 0 9)]
+               (if (not (has-value? board [x y]))
+                 (concat [x y])))) nil)))
+
+(defn sudoku-helper [board empty-set]
+  (if (empty? empty-set)
+    '()
+	  (for [value (valid-values-for board (first empty-set))
+	        target (set-value-at board (first empty-set) value)]
+	    (if (valid-solution? target)
+	      target
+	      (sudoku-helper target (rest empty-set)))
+	    ))
+  )
 
 (defn solve [board]
-  nil)
+  (if (filled? board)
+    (if (valid-solution? board)
+      board
+      [])
+    (let [coord (find-empty-point board)]
+      (for [value (valid-values-for board coord)
+            target (solve (set-value-at board coord value))]
+        target)))
+  )
