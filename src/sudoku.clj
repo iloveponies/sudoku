25d3920d118b646e61560b2e018c3aaa5b41c5cf
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-07-29 02:49:30 +0300

    fixed ex15

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -158,23 +158,23 @@
 
 
 (def sud-board
-  (board [[5 3 0 0 7 0 0 0 0]
-          [6 0 0 1 9 5 0 0 0]
-          [0 9 8 0 0 0 0 6 0]
-          [8 0 0 0 6 0 0 0 3]
-          [4 0 0 8 0 3 0 0 1]
-          [7 0 0 0 2 0 0 0 6]
-          [0 6 0 0 0 0 2 8 0]
-          [0 0 0 4 1 9 0 0 5]
-          [0 0 0 0 8 0 0 7 9]]))
+  (board [[0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]
+          [0 0 0 0 0 0 0 0 0]]))
 
 (defn solve [board]
-  (let [empty-point (find-empty-point board)]
+  (take 9 (let [empty-point (find-empty-point board)]
     (if (= nil empty-point)
       board
       (let [potentials (valid-values-for board empty-point)]
         (for [elem potentials
              solution (solve (set-value-at board empty-point elem))]
-        solution)))))
+        solution))))))
 
 (solve sud-board)
