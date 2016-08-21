eb91db61e74b81b319f98b21a296c342bd5e4818
Jake Hendrickson <jake.hendrickson@gmail.com>
2016-08-21 14:16:21 +0000

    Adds trace tools to project deps

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject sudoku "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [iloveponies.tests/sudoku "0.1.0-SNAPSHOT"]]
+                 [iloveponies.tests/sudoku "0.1.0-SNAPSHOT"]
+                 [org.clojure/tools.trace "0.7.9"]
+                 ]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
