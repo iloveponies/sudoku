2d0d8861b89f0bb229240ad6ecc1d59edf6891ec
Henri Suominen <erakoura@gmail.com>
2017-01-06 23:16:30 +0200

    first 6 exercises done

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,26 +3,35 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board [(get coord 0) (get coord 1)]))
 
 (defn has-value? [board coord]
-  nil)
+  (pos? (value-at board coord)))
 
 (defn row-values [board coord]
-  nil)
+  (let [[row _] coord]
+    (set (get board row))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[_ col] coord]
+    (set (map (fn [x] (value-at board [x col])) (range 0 9)))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [arr1 coords arr2 coords] (vec [arr1 arr2])))
 
 (defn block-values [board coord]
-  nil)
+  (let [[row col] coord
+        [block-top block-left] [(- row (mod row 3))(- col (mod col 3))]]
+   (set (map (fn [x] (value-at board x)) 
+      (for [x (range block-top  (+ block-top  3))
+            y (range block-left (+ block-left 3))]
+         (vec [x y]))))))
 
 (defn valid-values-for [board coord]
-  nil)
+  )
 
 (defn filled? [board]
   nil)
