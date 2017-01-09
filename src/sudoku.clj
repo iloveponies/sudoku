2ab25dde513363fa3bfa1fb0e7c8fb5426cda4ba
Charlotte <biyun.huang0769@gmail.com>
2017-01-09 19:44:59 +0300

    Submit exercises of 'Sudoku'

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,97 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (== 0 (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (set (get board (get coord 0))))
 
 (defn col-values [board coord]
-  nil)
+  (let [y (get coord 1)]
+    (set (reduce (fn [out a-seq] (cons (get a-seq y) out)) #{} board))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [rows coords
+        cols coords]
+    (conj [rows] cols)))
 
 (defn block-values [board coord]
-  nil)
+  (let [[x y] (map (fn [a] (* 3 (int (/ a 3)))) coord)
+        coords (for [r (conj [x] (+ 1 x) (+ 2 x))
+                      c (conj [y] (+ 1 y) (+ 2 y))]
+                 (conj [r] c))]
+    (set (reduce (fn [out c] (conj out (value-at board c))) [] coords)
+    )))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values (row-values board coord) (col-values board coord) (block-values board coord))))
 
 (defn filled? [board]
-  nil)
+  (let [coords (coord-pairs (range 9))]
+    (not (.contains (reduce (fn [out coord] (conj out (value-at board coord))) [] coords) 0))))
 
 (defn rows [board]
-  nil)
+  (let [xcoords (range 9)]
+    (reduce (fn [out x] (conj out (set (get board x)))) [] xcoords)))
 
 (defn valid-rows? [board]
-  nil)
+  (let [rows (rows board)]
+    (reduce (fn [out row] (and out (empty? (clojure.set/difference all-values row)))) true rows)))
 
 (defn cols [board]
-  nil)
+  (let [ycoords (range 9)]
+    (reduce (fn [out y] (conj out (col-values board (conj [0] y)))) [] ycoords)))
 
 (defn valid-cols? [board]
-  nil)
+  (let [cols (cols board)]
+    (reduce (fn [out col] (and out (empty? (clojure.set/difference all-values col)))) true cols)))
 
 (defn blocks [board]
-  nil)
+  (let [pairs (coord-pairs [0 3 6])]
+    (reduce (fn [out pair] (conj out (block-values board pair))) [] pairs)))
 
 (defn valid-blocks? [board]
-  nil)
+  (let [blocks (blocks board)]
+    (reduce (fn [out block] (and out (empty? (clojure.set/difference all-values block)))) true blocks)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (let [pairs (coord-pairs (range 9))]
+    (loop [pair pairs]
+      (cond (empty? pair)
+            nil
+            (== 0 (value-at board (first pair)))
+            (first pair)
+            :else
+            (recur (rest pair))))))
+
+(defn solve-helper [solutions current-board]
+  (let [empty-point (find-empty-point current-board)
+        valid-values (valid-values-for current-board empty-point)
+        current-solution (set-value-at current-board empty-point (first valid-values))]
+    (cond
+      (empty? valid-values)
+      solutions
+      (filled? current-solution)
+      (conj solutions current-solution)
+      :else
+      (let [remaining valid-values]
+        (for [elem remaining
+              solution (solve-helper solutions (set-value-at current-board empty-point elem))]
+          solution)))))
 
 (defn solve [board]
-  nil)
+  (first (solve-helper #{} board)))
