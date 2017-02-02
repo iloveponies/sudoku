cb33967c36801169467fb70bf7dbc04eb9f092aa
Pertti Husu <phusu@iki.fi>
2017-02-02 09:46:57 +0200

    Implemented all the functions.

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,144 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (== 0 (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[x y] coord]
+    (set (get board x))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[x y] coord]
+    (loop [result (set ())
+           index 0]
+      (if (< index 9)
+        (recur (conj result (value-at board [index y])) (inc index))
+        result))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [x coords
+        y coords]
+    [x y]))
+
+(defn block-top-left [coord]
+  (let [[x y] coord]
+    [(- x (mod x 3)) (- y (mod y 3))]))
 
 (defn block-values [board coord]
-  nil)
+  (let [top-left (block-top-left coord)
+        index-to-coord (fn [index] [(quot index 3) (mod index 3)])]
+    (loop [result (set ())
+          index 0]
+      (if (== index 9)
+        result
+        (recur (conj result (value-at board (map + top-left (index-to-coord index)))) (inc index))))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (not (== 0 (value-at board coord)))
+    (set ())
+    (set/difference all-values (block-values board coord) (row-values board coord) (col-values board coord))))
+
+(defn all-board-values [board]
+  (loop [result (set ())
+         index 0]
+    (if (== index 9)
+      result
+      (recur (set/union result (row-values board [index 0])) (inc index)))))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (all-board-values board) 0)))
 
 (defn rows [board]
-  nil)
+  (loop [result []
+         index 0]
+    (if (== index 9)
+      result
+      (recur (conj result (row-values board [index 0])) (inc index)))))
 
 (defn valid-rows? [board]
-  nil)
+  (loop [elem (first (rows board))
+         seq (rest (rows board))
+         index 0]
+    (cond 
+      (== index 9)
+        true
+      (not (== 9 (count elem)))
+        false
+      :else
+        (recur (first seq) (rest seq) (inc index)))))
 
 (defn cols [board]
-  nil)
+  (loop [result []
+         index 0]
+    (if (== index 9)
+      result
+      (recur (conj result (col-values board [0 index])) (inc index)))))
 
 (defn valid-cols? [board]
-  nil)
+  (loop [elem (first (cols board))
+         seq (rest (cols board))
+         index 0]
+    (cond 
+      (== index 9)
+        true
+      (not (== 9 (count elem)))
+        false
+      :else
+        (recur (first seq) (rest seq) (inc index)))))
 
 (defn blocks [board]
-  nil)
+  (let [index-to-coord (fn [index] 
+                        [(quot index 3) (mod index 9)])]
+    (loop [result []
+           index 0]
+      (if (== index 27)
+        result
+        (recur (conj result (block-values board (index-to-coord index))) (+ index 3))))))
 
 (defn valid-blocks? [board]
-  nil)
+  (loop [elem (first (blocks board))
+         seq (rest (blocks board))
+         index 0]
+    (cond 
+      (== index 9)
+        true
+      (not (== 9 (count elem)))
+        false
+      :else
+        (recur (first seq) (rest seq) (inc index)))))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-cols? board) (valid-rows? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (let [index-to-coord (fn [index] 
+                        [(quot index 9) (mod index 9)])]
+    (loop [index 0]
+      (cond 
+        (== index 81)
+          nil
+        (== 0 (value-at board (index-to-coord index)))
+          (index-to-coord index)
+        :else
+          (recur (inc index))))))
 
 (defn solve [board]
-  nil)
+  (let [index (find-empty-point board)]
+    (cond
+      (= index nil)
+        (if (valid-solution? board)
+          board
+          ())
+      :else
+        (for [elem (valid-values-for board index)
+              candidate (solve (set-value-at board index elem))]
+          candidate))))
