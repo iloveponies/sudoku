bc76ebb4ffc0143fa92dc7c9908f9cd4ad611ad4
Hantula Otto A <ottohant@da3-cs-dk110-07.pc.helsinki.fi>
2017-03-14 13:48:08 +0200

    gfs

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,50 +3,93 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (set (get board (first coord))))
 
 (defn col-values [board coord]
-  nil)
+  (let [helper (fn [nums elem]
+                 (conj nums (get elem (second coord))))]
+    (reduce helper #{} board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [x coords
+        y coords]
+    [x y]))
 
 (defn block-values [board coord]
-  nil)
+  (let [top-left (fn [x]
+                   (cond
+                     (< x 3)
+                       0
+                     (< x 6)
+                       3
+                     :else
+                       6))
+        top-left-x (top-left (first coord))
+        top-left-y (top-left (second coord))
+        values (for [x [top-left-x (+ 1 top-left-x) (+ 2 top-left-x)]
+                     y [top-left-y (+ 1 top-left-y) (+ 2 top-left-y)]]
+                 (value-at board [x y]))]
+    (set values)))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values
+                    (block-values board coord)
+                    (row-values board coord)
+                    (col-values board coord))))
 
 (defn filled? [board]
-  nil)
+  (let [helper (fn [nums elem]
+                 (apply conj nums elem))]
+    (not (contains? (reduce helper #{} board) 0))))
 
 (defn rows [board]
-  nil)
+  (let [halp (fn [rows row]
+               (conj rows (set row)))]
+    (reduce halp [] board)))
 
 (defn valid-rows? [board]
-  nil)
+  (let [checker (fn [row]
+                  (and (not (contains? row 0))
+                       (empty? (set/difference all-values row))))
+        the-rows (rows board)]
+    (every? checker the-rows)))
 
 (defn cols [board]
-  nil)
+  (for [col [0 1 2 3 4 5 6 7 8]]
+        (col-values board [0 col])))
 
 (defn valid-cols? [board]
-  nil)
+  (let [checker (fn [col]
+                  (and (not (contains? col 0))
+                       (empty? (set/difference all-values col))))
+        the-cols (cols board)]
+    (every? checker the-cols)))
 
 (defn blocks [board]
-  nil)
+  (for [x [0 3 6]
+        y [0 3 6]]
+    (block-values board [x y])))
 
 (defn valid-blocks? [board]
-  nil)
+  (let [checker (fn [block]
+                  (and (not (contains? block 0))
+                       (empty? (set/difference all-values block))))
+        the-blocks (blocks board)]
+    (every? checker the-blocks)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
   nil)
