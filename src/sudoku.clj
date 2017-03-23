542ce14d295e151443db18e880dfb5d3cddf2455
Paul Vuorela <vuorpa13@gmail.com>
2017-03-23 13:46:18 +0200

    Sudoku solver done

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,56 +3,82 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (= (value-at board coord) 0)))
 
 (defn row-values [board coord]
-  nil)
+  (set (get board (get coord 0))))
 
 (defn col-values [board coord]
-  nil)
+  (reduce (fn [s x] (conj s (get x (get coord 1)))) #{} board))
 
-(defn coord-pairs [coords]
-  nil)
+(defn coord-pairs
+   ([coords] (for [x coords y coords] [x y]))
+   ([coordx coordy] (for [x coordx y coordy] [x y])))
+
+(defn topleft-corner [coord]
+  [(* (int (/ (get coord 0) 3)) 3) (* (int (/ (get coord 1) 3)) 3)])
 
 (defn block-values [board coord]
-  nil)
+  (let [[s t] [(get (topleft-corner coord) 0) (get (topleft-corner coord) 1)]]
+    (reduce (fn [x y] (conj x (value-at board y))) #{}
+            (coord-pairs (range s (+ s 3)) (range t (+ t 3))))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (not (= (value-at board coord) 0))
+    #{}
+    (set/difference all-values
+                    (set/union (row-values board coord)
+                               (col-values board coord)
+                               (block-values board coord)))))
+
+(defn numbers-on-board [board]
+  (set (reduce set/union #{} board)))
 
 (defn filled? [board]
-  nil)
+  (not (contains? (numbers-on-board board) 0)))
 
 (defn rows [board]
-  nil)
+  (reduce (fn [s x] (conj s (set x))) [] board))
 
 (defn valid-rows? [board]
-  nil)
+  (empty? (filter (fn [x] (not (= x all-values))) (rows board))))
 
 (defn cols [board]
-  nil)
+  (reduce (fn [s x] (conj s (col-values board [0 x]))) [] (range 9)))
 
 (defn valid-cols? [board]
-  nil)
+  (empty? (filter (fn [x] (not (= x all-values))) (cols board))))
 
 (defn blocks [board]
-  nil)
+  (let [corners (coord-pairs [0 3 6])]
+    (reduce (fn [s x] (conj s (block-values board x))) [] corners)))
 
 (defn valid-blocks? [board]
-  nil)
+  (empty? (filter (fn [x] (not (= x all-values))) (blocks board))))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (first (filter (fn [x] (= 0 (value-at board x))) (coord-pairs (range 9)))))
+
+(defn solve-helper [current-board]
+  (let [remaining (find-empty-point current-board)]
+    (if (nil? remaining)
+      (if (valid-solution? current-board)
+        [current-board] [])
+      (for [value (valid-values-for current-board remaining)
+            solution (solve-helper (set-value-at current-board remaining value))]
+        solution))))
 
 (defn solve [board]
-  nil)
+  (first (solve-helper board)))
