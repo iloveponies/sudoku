da16cb7d87a868b0e9add74aea0ac11d3989146d
Lucas Mancini <mancini.lucas@gmail.com>
2017-04-03 18:20:50 -0300

    Implementing set of function until "filled?", to check whether the board is full or not.

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -1,31 +1,63 @@
 (ns sudoku
   (:require [clojure.set :as set]))
 
+; Definitions of the data structures and helper functions
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
+; Functions to implement that make tests pass:
 (defn value-at [board coord]
-  nil)
+ (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+ (not (= 0 (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+ (let [[row-number _] coord
+       row (get board row-number)]
+  (set row)))
 
 (defn col-values [board coord]
-  nil)
+ (let [[_ col-number] coord
+       column (map #(get % col-number) board)]
+  (set column)))
 
 (defn coord-pairs [coords]
-  nil)
+ (for [row coords
+       col coords]
+  [row col]))
+
+(defn corner-number [n]
+  (cond
+   (<= 0 n 2) 0
+   (<= 3 n 5) 3
+   (<= 6 n 8) 6
+   :else false))
+
+(defn block-corner [coord]
+ (map corner-number coord))
 
 (defn block-values [board coord]
-  nil)
+ (let [[coord-corner-x coord-corner-y] (block-corner coord)
+       block-values-list (for [x (range coord-corner-x (+ coord-corner-x 3))
+                               y (range coord-corner-y (+ coord-corner-y 3))]
+                          (value-at board [x y]))]
+  (set block-values-list)))
 
 (defn valid-values-for [board coord]
-  nil)
+ (if (has-value? board coord)
+  #{}
+  (set/difference all-values
+                  (row-values board coord)
+                  (col-values board coord)
+                  (block-values board coord))))
 
 (defn filled? [board]
-  nil)
+ (let [all-values-concat (reduce concat [] board)
+       all-values-concat-set (set all-values-concat)
+       board-has-empty-value (contains? all-values-concat-set 0)]
+  (not board-has-empty-value)))
 
 (defn rows [board]
   nil)
