6dd67c67b82c0a5ae40af4faf1c04849db53522d
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-04-03 16:33:49 +0300

    implement value-at and has-value?

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,11 +3,13 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
 (defn row-values [board coord]
   nil)
