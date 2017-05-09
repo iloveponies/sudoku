e22ceea056115ba742d236184d37a26cbf698057
Lea Subrenat <subrlea@yahoo.fr>
2017-05-09 10:01:50 +0100

    Sudoku solved

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,55 +4,100 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (if (= (value-at board coord) 0) false true))
 
 (defn row-values [board coord]
-  nil)
+  (let [x (first coord)
+        row (get board x)]
+    (set row)))
 
 (defn col-values [board coord]
-  nil)
+  (let [y (second coord)]
+    (loop [cols board
+           values nil]
+      (if (empty? (first cols))
+        (set values)
+        (recur (rest cols) (conj values (get (first cols) y)))))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [x coords
+        y coords]
+    (vector x y)))
+
+(defn top-left [board coord]
+  (map (fn [i] (* (int (/ i 3)) 3)) coord))
 
 (defn block-values [board coord]
-  nil)
+  (let [top-left-coord (top-left board coord)
+        x (first top-left-coord)
+        y (second top-left-coord)
+        x-range (range x (+ x 3))
+        y-range (range y (+ y 3))
+        values (for [x-value x-range
+                     y-value y-range]
+                 (value-at board (vector x-value y-value)))]
+    (set values)))
+
+(def all-values #{1 2 3 4 5 6 7 8 9})
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (clojure.set/difference all-values
+                            (clojure.set/union (col-values board coord)
+                                               (row-values board coord)
+                                               (block-values board coord)))))
 
 (defn filled? [board]
-  nil)
+  (let [all-values (for [x (range 9)
+                         y (range 9)]
+                     (value-at board [x y]))]
+    (if (some zero? all-values)
+      false
+      true)))
 
 (defn rows [board]
-  nil)
+  (for [x (range 9)]
+    (row-values board [x 0])))
+
+(defn valid? [x]
+  (if (= x #{1 2 3 4 5 6 7 8 9}) true false))
 
 (defn valid-rows? [board]
-  nil)
+  (if (every? valid? (rows board)) true false))
 
 (defn cols [board]
-  nil)
+  (for [y (range 9)]
+    (col-values board [0 y])))
 
 (defn valid-cols? [board]
-  nil)
+  (if (every? valid? (cols board)) true false))
 
 (defn blocks [board]
-  nil)
+  (for [x [0 3 6]
+        y [0 3 6]]
+    (block-values board [x y])))
 
 (defn valid-blocks? [board]
-  nil)
+  (if (every? valid? (blocks board)) true false))
 
 (defn valid-solution? [board]
-  nil)
+  (if (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)) true false))
 
 (defn set-value-at [board coord new-value]
-  nil)
+  (assoc-in board coord new-value))
 
 (defn find-empty-point [board]
-  nil)
+  (first (remove (fn [coord] (has-value? board coord)) (coord-pairs (range 9)))))
 
 (defn solve [board]
-  nil)
+  (if-let [point (find-empty-point board)]
+    (let [valid-values (valid-values-for board point)]
+      (for [value valid-values
+            solution (solve (set-value-at board point value))]
+        solution))
+    (if (valid-solution? board)
+      (first [board]))))
