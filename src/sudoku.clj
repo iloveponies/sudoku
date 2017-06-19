5c08af4eaa15512f30629b7f7df6216d3522159d
Lilja <triforce@windowslive.com>
2017-06-19 19:04:37 +0300

    exercises 1-6

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,22 +4,52 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (zero? (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[& row] (get board (get coord 0))]
+  (set row)))
 
 (defn col-values [board coord]
-  nil)
+  (loop [row 0
+         col-vals #{}]
+    (let [col-val (value-at board [row (get coord 1)])]
+      (if (= 9 row)
+        col-vals
+        (recur (inc row) (conj col-vals col-val))))))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [c1 coords
+        c2 coords]
+   (vector c1 c2)))
+
+(defn block-corner [coord]
+  (let [row (get coord 0)
+        col (get coord 1)]
+    (cond
+      (and (<= 0 row 2) (<= 0 col 2)) [0 0] ; top-left
+      (and (<= 0 row 2) (<= 3 col 5)) [0 3] ; top-middle
+      (and (<= 0 row 2) (<= 6 col 8)) [0 6] ; top-right
+      (and (<= 3 row 5) (<= 0 col 2)) [3 0] ; middle-left
+      (and (<= 3 row 5) (<= 3 col 5)) [3 3] ; center
+      (and (<= 3 row 5) (<= 6 col 8)) [3 6] ; middle-right
+      (and (<= 6 row 8) (<= 0 col 2)) [6 0] ; bottom-left
+      (and (<= 6 row 8) (<= 3 col 5)) [6 3] ; bottom-middle
+      :else [6 6]))) ; bottom-right
+
+(defn block-coords [coord]
+  (let [top-left (block-corner coord)
+        base-coords (coord-pairs [0 1 2])
+        inc-coords (fn [coords coord] (conj coords (map + coord top-left)))]
+    (reduce inc-coords [] base-coords)))
 
 (defn block-values [board coord]
-  nil)
+  (let [block (block-coords coord)
+        values (fn [res c] (conj res (value-at board c)))]
+    (reduce values #{} block)))
 
 (defn valid-values-for [board coord]
   nil)
