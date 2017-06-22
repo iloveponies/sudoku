8d7f146142d85838fe7447dd19990926bd4a159d
Peter Hayes <peterhayes1@hotmail.com>
2017-06-23 00:19:37 +0100

    Code tidy

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -1,7 +1,9 @@
 (ns sudoku
   (:require [clojure.set :as set]))
 
+
 (def board identity)
+
 
 (def worlds-hardest-board ;; according to: http://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
   (board [[8 0 0 0 0 0 0 0 0]
@@ -14,40 +16,49 @@
           [0 0 8 5 0 0 0 1 0]
           [0 9 0 0 0 0 4 0 0]]))
 
-;Ex. 1
+
+;; Ex 1
+;; Write the function (value-at board coordinates) that returns the value at coordinate in board:
 
 (defn value-at [board coord]
   (let [[row col] coord]
     (get-in board [row col])))
 
 
-;Ex. 2
+;; Ex 2
+;; Write the function (has-value? board coordinates) that returns false if the square at coordinates is empty (has 0), and otherwise true.
 
 (defn has-value? [board coord]
   (not= 0 (value-at board coord)))
 
 
-;Ex. 3
+;; Ex 3
+;; Write the function (row-values board coordinates) that returns a set with all numbers on the row of the coordinates
+;; Remember that you can use destructing inside the parameter vector to get the row.
 
 (defn row-values [board coord]
   (let [[row _] coord]
    (into #{} (nth board row))))
 
 
-;Ex. 4
+;; Ex 4
+;; Write the function (col-values board coordinates) that returns a set with all numbers on the col of the coordinates
 
 (defn col-values [board coord]
   (let [[_ col] coord]
     (into #{} (map #(nth % col) board))))
 
 
-;Ex. 5
+;; Ex 5
+;; Write the function (coord-pairs coord-sequence) that returns all coordinate-pairs of the form [row col] where row is from coord-sequence and col is from coord-sequence.
 
 (defn coord-pairs [coord-sequence]
   (for [x coord-sequence y coord-sequence] [x y]))
 
 
-;Ex: 6
+;; Ex 6
+;; Write the function (block-values board coordinates) that returns a set with all numbers in the block of coordinates.
+;; You might want to write a helper function that returns the coordinates for the top left corner of the block.
 
 ; Helpers:
 ; Use defs here as they are evaluated only once.
@@ -70,7 +81,10 @@
   (set (map #(value-at board %) (coord->coordlist coord))))
 
 
-;Ex: 7
+;; Ex 7
+;; Write the function (valid-values-for board coordinates) that returns a set with all valid numbers for the square at coordinates.
+;; If the square at coordinates already has a value, valid-values should return the empty set #{}.
+;; Remember that we already defined the set all-values.
 
 (defn valid-values-for [board coord]
   (if (not (zero? (value-at board coord)))
@@ -78,13 +92,17 @@
     (set/difference all-values (set/union (block-values board coord) (row-values board coord) (col-values board coord)))))
 
 
-;Ex: 8
+;; Ex 8
+;; Write the function (filled? board) which returns true if there are no empty squares in board, and otherwise false.
+;; It might help to write a helper function that returns all numbers of the board in a sequence.
+;; Remember that (contains? set element) can be used to check if element is in set.
 
 (defn filled? [board]
   (not (contains? (reduce (fn [s row] (set/union s (set row))) #{} board) 0)))
 
 
-;Ex: 9
+;; Ex 9
+;; Write the function (rows board) that returns a sequence of value sets for each row of board. That is, the first set in (rows board) is a set that has every element of the first row of board as element and so on.
 
 (defn rows [board]
   (apply vector (map #(into #{} %) board)))
@@ -95,38 +113,49 @@
     (set (map #(nth % col) board)))))
 
 
-;Ex: 10
+;; Ex 10
+;; Write the function (blocks board) that returns the values of each block in board as a sequence of sets.
 
 (defn blocks [board]
   (apply vector (for [coord block->coordlist]
     (set (map (partial value-at board) (nth coord 1))))))
 
 
-;Ex: 11
+;; Ex 11
+;; Write the function (valid-rows? board) that returns true if every row in board is a valid filled row.
 
 (defn valid-rows? [board]
   (every? #(= all-values (set %)) (rows board)))
 
+;; Ex 11 - contd.
+;; Write the function (valid-cols? board) that returns true if every row in board is a valid filled column.
+
 (defn valid-cols? [board]
   (every? #(= all-values (set %)) (cols board)))
+
+;; Ex 11 - contd.
+;; Write the function (valid-blocks? board) that returns true if every block in board is a valid filled block.
 
 (defn valid-blocks? [board]
   (every? #(= all-values (set %)) (blocks board)))
 
 
-; Ex: 12
+;; Ex 12
+;; Write the function (valid-solution? board) that returns true if board is a valid solution to sudoku.
 
 (defn valid-solution? [board]
   (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 
-; Ex: 13
+;; Ex 13
+;; Write the function (set-value-at board coord new-value) that changes the value at coord in board to new-value
 
 (defn set-value-at [board coord new-value]
   (assoc-in board coord new-value))
 
 
-; Ex: 14
+;; Ex 14
+;; Write the function (find-empty-point board) that returns coordinates to an empty point (that is, in our representation has value 00).
 
 (defn find-empty-point [board]
   (reduce
@@ -137,7 +166,18 @@
     (for [row (range 9) col (range 9)] [row col])))
 
 
-; Ex: 15
+;; Ex 15
+;; Write the function (solve board) which takes a sudoku board as a parameter and returns a valid solution to the given sudoku.
+;;
+;; Recap of backtracking:
+;;
+;;   . check if you are at the end
+;;   . if so, is the solution valid?
+;;     . if not, return an empty sequence
+;;     . otherwise return [solution]
+;;   . if not
+;;     . select an empty location
+;;     . try solving with each valid value for that location
 
 (defn get-move-list
   "Helper to generate a list of new board positions"
