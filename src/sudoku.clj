5c0f647fb98757c1d07d4f9e1b3660f41a9a207b
rkelkka <raine_kelkka@hotmail.com>
2018-12-16 20:26:33 +0200

    Up-to block-values

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -1,7 +1,8 @@
 (ns sudoku
   (:require [clojure.set :as set]))
 
-;(def board2 [[5 3 0 0 7 0 0 0 0]
+;(def board2
+;       [[5 3 0 0 7 0 0 0 0]
 ;        [6 0 0 1 9 5 0 0 0]
 ;        [0 9 8 0 0 0 0 6 0]
 ;        [8 0 0 0 6 0 0 0 3]
@@ -10,6 +11,18 @@
 ;        [0 6 0 0 0 0 2 8 0]
 ;        [0 0 0 4 1 9 0 0 5]
 ;        [0 0 0 0 8 0 0 7 9]])
+
+;[[5 3 0 | 0 7 0 | 0 0 0]
+; [6 0 0 | 1 9 5 | 0 0 0]
+; [0 9 8 | 0 0 0 | 0 6 0]
+; -------+-------+-------
+; [8 0 0 | 0 6 0 | 0 0 3]
+; [4 0 0 | 8 0 3 | 0 0 1]
+; [7 0 0 | 0 2 0 | 0 0 6]
+; -------+-------+-------
+; [0 6 0 | 0 0 0 | 2 8 0]
+; [0 0 0 | 4 1 9 | 0 0 5]
+; [0 0 0 | 0 8 0 | 0 7 9]]
 
 (def board identity)
 
@@ -32,10 +45,41 @@
     (set (map (fn [row] (get-in board [row col])) (range (count board))))))
 
 (defn coord-pairs [coords]
-  nil)
+  (let [pairs []]
+   (for [row coords col coords]
+     (seq (conj pairs row col)))))
+
+;assumes 3x3 board
+;returns [x,y]
+(defn block-top-left [coord]
+  (let [[row _] coord [_ col] coord]
+    (let
+      [x
+       (cond
+        (contains? #{0 1 2} row)
+         0
+        (contains? #{3 4 5} row)
+         3
+        (contains? #{6 7 8} row)
+         6)
+      y
+       (cond
+        (contains? #{0 1 2} col)
+         0
+        (contains? #{3 4 5} col)
+         3
+        (contains? #{6 7 8} col)
+         6)]
+    (vector x y))))
+
 
 (defn block-values [board coord]
-  nil)
+  (let [[top-left-x _] (block-top-left coord)
+        [ _ top-left-y] (block-top-left coord)]
+   (let [x-coords (range top-left-x (+ top-left-x 3))
+         y-coords (range top-left-y (+ top-left-y 3))]
+    (set (for [row x-coords col y-coords]
+      (value-at board (vector row col)))))))
 
 (defn valid-values-for [board coord]
   nil)
