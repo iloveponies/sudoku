69cfed5b058e6d88f2eab1e300284a0945a85ba8
rkelkka <raine_kelkka@hotmail.com>
2018-12-16 13:53:48 +0200

    Up to Ex 4

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -1,19 +1,35 @@
 (ns sudoku
   (:require [clojure.set :as set]))
 
+;(def board2 [[5 3 0 0 7 0 0 0 0]
+;        [6 0 0 1 9 5 0 0 0]
+;        [0 9 8 0 0 0 0 6 0]
+;        [8 0 0 0 6 0 0 0 3]
+;        [4 0 0 8 0 3 0 0 1]
+;        [7 0 0 0 2 0 0 0 6]
+;        [0 6 0 0 0 0 2 8 0]
+;        [0 0 0 4 1 9 0 0 5]
+;        [0 0 0 0 8 0 0 7 9]])
+
 (def board identity)
 
+
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (= 0 (value-at board coord))))
 
+;returns set i.e. from multiple zeroes only one is included
 (defn row-values [board coord]
-  nil)
+  (let [[row] coord]
+    (set (get board row))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[_ col] coord]
+    (set (map (fn [row] (get-in board [row col])) (range (count board))))))
 
 (defn coord-pairs [coords]
   nil)
